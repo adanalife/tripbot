cf3f09e8ec79eab651a03ab0f941f538b9ded8e4
Dana Merrick <dana@snapdocs.com>
2018-08-02 11:16:53 -0400

    Not working

--- start-stream.sh
+++ start-stream.sh
@@ -13,12 +13,12 @@ ffmpeg \
   -bufsize 4200k \
   -framerate 15 \
   -filter_complex \
-  "[0:v]crop=180:50:0:in_h-50,boxblur=10[fg]; \
-   [0:v][fg]overlay=0:main_h-overlay_h[v]" \
+    "[0:v]crop=180:50:0:in_h-out_h,boxblur=10[fg]; \
+     [0:v][fg]overlay=0:main_h-overlay_h[v]" \
   -map "[v]" \
   -c:a copy \
   -c:v libx264 \
-  -preset ultrafast \
+  -preset slow \
   -pix_fmt yuv420p \
   -s 1280x720 \
   -g 120 \
