4705bd6e91ff8b8c30ba994996a9493b7b4c5849
Dana Merrick <dana@snapdocs.com>
2018-08-02 10:58:13 -0400

    Not working but box looks good

--- start-stream.sh
+++ start-stream.sh
@@ -13,10 +13,10 @@ ffmpeg \
   -bufsize 4200k \
   -framerate 15 \
   -filter_complex \
-  "[0:v]crop=180:50:0:in_h-180,boxblur=10[fg]; \
+  "[0:v]crop=180:50:0:in_h-50,boxblur=10[fg]; \
    [0:v][fg]overlay=0:main_h-overlay_h[v]" \
   -map "[v]" \
-  -acodec copy \
+  -c:a copy \
   -c:v libx264 \
   -preset ultrafast \
   -pix_fmt yuv420p \
