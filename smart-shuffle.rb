be957065faf76d902f5e85486d12b37dd48e7116
Dana Merrick <dana@snapdocs.com>
2018-08-03 14:23:38 -0400

    Initial commit

---
+++ smart-shuffle.rb
@@ -0,0 +1,28 @@
+#!/usr/bin/env ruby
+
+# use the dir passed in via CLI
+vid_dir = ARGV.shift
+
+# find all the files in the dir
+all_files = Dir.glob("#{vid_dir}/**/*")
+# but only incude MP4 files
+files = all_files.select{|f| f =~ /\.MP4$/ }
+
+# create a hash that organizes files by date taken
+by_day = {}
+files.each do |file|
+  date = file.sub(/.*\//,'')[0..8]
+  by_day[date] ||= []
+  by_day[date] << file
+end
+
+# since some files are in a "read-only" subdir, make sure
+# those are in the correct order when sorted
+by_day.each do |date, file_list|
+  file_list.sort! do |a,b|
+    a.sub(/\/RO/,'') <=> b.sub(/\/RO/,'')
+  end
+end
+
+require 'pp'
+pp by_day
