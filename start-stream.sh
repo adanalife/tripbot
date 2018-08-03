201578fb3878841c8a2835210bf7c967aec0d9fe
Dana Merrick <dana@snapdocs.com>
2018-08-03 15:13:07 -0400

    Going back to working version for a bit

--- start-stream.sh
+++ start-stream.sh
@@ -16,28 +16,41 @@ VID_DIR="$2"
 #TODO: check if we have ffmpeg and fail gracefully
 
 ffmpeg \
-  -hide_banner \
-  -safe 0 \
   -f concat \
-  -i <(./smart-shuffle.rb $VID_DIR) \
+  -safe 0 \
+  -i <(for f in $VID_DIR/*.MP4; do echo "file '$f'"; done | sort -R) \
   -s 1920x1080 \
-  -maxrate 6000k \
-  -bufsize 4200k \
   -framerate 15 \
-  -filter_complex \
-    "[0:v]crop=180:50:0:in_h-out_h,boxblur=10[fg]; \
-     [0:v][fg]overlay=0:main_h-overlay_h[v]" \
-  -map "[v]" \
-  -c:a copy \
+  -an \
   -c:v libx264 \
-  -preset slow \
+  -preset ultrafast \
   -pix_fmt yuv420p \
-  -s 1280x720 \
-  -r 30 \
-  -g 60 \
-  -crf 40 \
-  -f flv \
-  "rtmp://live.twitch.tv/app/$STREAM_KEY"
+  -s 1920x1080 \
+  -f flv "rtmp://live.twitch.tv/app/$STREAM_KEY"
+
+# ffmpeg \
+#   -hide_banner \
+#   -safe 0 \
+#   -f concat \
+#   -i <(./smart-shuffle.rb $VID_DIR) \
+#   -s 1920x1080 \
+#   -maxrate 6000k \
+#   -bufsize 4200k \
+#   -framerate 15 \
+#   -filter_complex \
+#     "[0:v]crop=180:50:0:in_h-out_h,boxblur=10[fg]; \
+#      [0:v][fg]overlay=0:main_h-overlay_h[v]" \
+#   -map "[v]" \
+#   -c:a copy \
+#   -c:v libx264 \
+#   -preset slow \
+#   -pix_fmt yuv420p \
+#   -s 1280x720 \
+#   -r 30 \
+#   -g 60 \
+#   -crf 40 \
+#   -f flv \
+#   "rtmp://live.twitch.tv/app/$STREAM_KEY"
 
   # -preset slower \
   #-c:v mpeg2video \
