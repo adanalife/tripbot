63d12b8106d624f72b964ca489b644a7ce885396
Dana Merrick <dana@snapdocs.com>
2018-08-03 13:04:10 -0700

    Working but laggy

--- start-stream.sh
+++ start-stream.sh
@@ -25,44 +25,44 @@ ffmpeg \
      [0:v][fg]overlay=0:main_h-overlay_h[v]" \
   -map "[v]" \
   -s 1920x1080 \
-  -framerate 15 \
   -an \
   -c:v libx264 \
   -preset ultrafast \
+  -x264-params "nal-hrd=cbr" \
+  -r 30 \
   -pix_fmt yuv420p \
-  -s 1280x720 \
   -crf 28 \
   -f flv \
   "rtmp://live.twitch.tv/app/$STREAM_KEY"
 
 
 exit 0
+  -s 1280x720 \
   -r 30 \
 
   -maxrate 12M \
   -bufsize 6M \
 # safe to put old configs down here cause we wont ever get down here
 
-# ffmpeg \
-#   -s 1920x1080 \
-#   -framerate 15 \
-#   -c:a copy \
-#   -c:v libx264 \
-#   -preset slow \
-#   -pix_fmt yuv420p \
-#   -g 60 \
-#   -f flv \
-#   "rtmp://live.twitch.tv/app/$STREAM_KEY"
+ffmpeg \
+  -s 1920x1080 \
+  -framerate 15 \
+  -c:a copy \
+  -c:v libx264 \
+  -preset slow \
+  -pix_fmt yuv420p \
+  -g 60 \
+  -f flv \
+  "rtmp://live.twitch.tv/app/$STREAM_KEY"
 
-  # -preset slower \
-  #-c:v mpeg2video \
-  #-loglevel info \
-#  -vcodec copy \
-  # -x264-params "nal-hrd=cbr" \
-  # -b:v 1M \
-  # -minrate 1M \
-  # -maxrate 1M \
-  # -bufsize 2M \
-  # -profile:v baseline \
-  # -level 3.0 \
-  # -movflags +faststart \
+  -preset slower \
+  -c:v mpeg2video \
+  -loglevel info \
+  -vcodec copy \
+  -b:v 1M \
+  -minrate 1M \
+  -maxrate 1M \
+  -bufsize 2M \
+  -profile:v baseline \
+  -level 3.0 \
+  -movflags +faststart \
