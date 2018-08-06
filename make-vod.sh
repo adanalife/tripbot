0f57a2632a7af56145795b597d1e02092c13fd9e
Dana Merrick <dana@snapdocs.com>
2018-08-05 22:36:07 -0400

    Adding a VOD maker

---
+++ make-vod.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+# This script takes a directory of videos and streams them to Twitch
+#
+# I use it for streaming dashcam footage, and because of this I apply
+# a filter to blur out some a portion of the screen.
+
+#TODO: check if we have ffmpeg and fail gracefully
+#TODO: the blur needs to not cover the E/W
+
+if [[ $# -eq 0 ]] ; then
+  echo "Usage: $0 [dir containing vids]"
+  exit 1
+fi
+
+VID_DIR="$1"
+
+ffmpeg \
+  -hide_banner \
+  -i "$VID_DIR/$(ls $VID_DIR | sort -R | head -1)" \
+  -filter_complex \
+    "[0:v]crop=130:46:25:in_h-out_h-10,boxblur=10[fg]; \
+     [0:v][fg]overlay=25:main_h-overlay_h-10[v] "\
+  -map "[v]" \
+  -s 1920x1080 \
+  -an \
+  -c:v libx264 \
+  -f flv \
+  -r 60 \
+  -t 21600 \
+  test.flv
+
+####################################################################
+# safe to put old configs down here cause we wont ever get down here
+####################################################################
+
+exit 0
+
+     movie=/Users/dmerrick/other_projects/stream/logo-fade-animation.gif[inner]; \
+     [v][inner]overlay=70:70[v]" \
