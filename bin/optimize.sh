91aefd3116f996ae95f2bf534ac50f25d579a1dd
Dana Merrick <dana.merrick@postmates.com>
2019-04-30 19:17:12 -0500

    Improvements

--- bin/optimize.sh
+++ bin/optimize.sh
@@ -5,8 +5,11 @@ file=$1
 no_extension=${file%.MP4}
 just_file=${no_extension##*/}
 
-ffmpeg -i $1 -c:v libx264 -x264opts keyint=2:no-scenecut -s 1920x1080 -r 60 -b:v 5000 -an ${just_file}_opt.MP4
+#ffmpeg -i $1 -preset slow -crf 21 -bufsize 10000k -maxrate 5000k -pix_fmt yuv420p -c:v libx264 -x264opts keyint=2:no-scenecut -s 1920x1080 -r 60 -an ${just_file}_opt.MP4
+ffmpeg -i $1 -preset veryslow -crf 15 -bufsize 20000k -maxrate 10000k -pix_fmt yuv420p -c:v libx264 -x264opts keyint=2:no-scenecut -s 1920x1080 -r 60 -an ${just_file}_opt.MP4
 
+
+# -preset slow
 # -profile:v <profile>
 # -sws_flags <scaler algorithm>
 # -hls_list_size <number of playlist entries>
