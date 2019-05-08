5c7bf1590086597c49173037757654a79a017732
Dana Merrick <dana.merrick@postmates.com>
2019-05-07 20:42:06 -0500

    Might as well commit my progress

--- tripbot4000.go
+++ tripbot4000.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"encoding/gob"
 	"fmt"
 	"log"
 	"os"
@@ -11,8 +13,10 @@ import (
 )
 
 const (
-	clientUsername = "TripBot4000"
-	channelToJoin  = "adanalife_"
+	clientUsername    = "TripBot4000"
+	channelToJoin     = "adanalife_"
+	userJoinsBucket   = "user_joins"
+	userWatchedBucket = "user_watched"
 )
 
 // these are other bots who shouldn't get points
@@ -23,9 +27,49 @@ var ignoredUsers = []string{
 	"logviewer",
 }
 
+type Store struct {
+	path string
+	db   *bolt.DB
+}
+
+func NewStore(path string) *Store {
+	return &Store{
+		path: path,
+	}
+}
+
+// Open opens and initializes the database.
+func (s *Store) Open() error {
+	// Open underlying data store.
+	db, err := bolt.Open(s.path, 0666, &bolt.Options{Timeout: 1 * time.Second})
+	if err != nil {
+		return err
+	}
+	s.db = db
+
+	// Initialize all the required buckets.
+	if err := s.db.Update(func(tx *bolt.Tx) error {
+		tx.CreateBucketIfNotExists([]byte(userJoinsBucket))
+		tx.CreateBucketIfNotExists([]byte(userWatchedBucket))
+		return nil
+	}); err != nil {
+		s.Close()
+		return err
+	}
+
+	return nil
+}
+
+func (s *Store) Close() error {
+	if s.db != nil {
+		s.db.Close()
+	}
+	return nil
+}
+
 // datastore for user joins
-var userJoins map[string]time.Time = make(map[string]time.Time)
-var userWatched map[string]time.Duration = make(map[string]time.Duration)
+// var userJoins map[string]time.Time = make(map[string]time.Time)
+// var userWatched map[string]time.Duration = make(map[string]time.Duration)
 
 // returns true if a given user should be ignored
 func userIsIgnored(user string) bool {
@@ -37,19 +81,68 @@ func userIsIgnored(user string) bool {
 	return false
 }
 
-func recordUserJoin(user string) {
-	userJoins[user] = time.Now()
+func (s *Store) recordUserJoin(user string) {
 	log.Println(user, "joined the channel")
+
+	s.db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(userJoinsBucket))
+		currentTime, err := time.Now().MarshalText()
+		err = b.Put([]byte(user), []byte(currentTime))
+		return err
+	})
 }
 
-func recordUserPart(user string) {
-	if joinTime, ok := userJoins[user]; ok {
-		duration := time.Since(joinTime)
-		userWatched[user] += duration
-	} else {
-		log.Println("Hmm, part message with no join for user", user)
-	}
-	log.Println(user, "left the channel, total watched:", fmtDuration(userWatched[user]))
+func (s *Store) recordUserPart(user string) {
+	var joinTime time.Time
+	var previousDurationWatched time.Duration
+
+	// first find the time the user joined the channel
+	s.db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(userJoinsBucket))
+		joinTime = time.Now()
+		err := joinTime.UnmarshalText(b.Get([]byte(user)))
+		return err
+	})
+
+	// if we didn't find anything, we can't continue
+	// if joinTime == nil {
+	// 	return err
+	// }
+
+	// seems like we did find a time, so calculate the duration watched
+	durationWatched := time.Since(joinTime)
+
+	// fetch the previous duration watched from the DB
+	s.db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(userWatchedBucket))
+		previousDurationWatched, err := time.ParseDuration(string(b.Get([]byte(user))))
+		return err
+	})
+
+	// maybe they're a new user
+	// if previousDurationWatched == 0 {
+	// 	previousDurationWatched = 0
+	// }
+
+	// calculate total duration watched
+	totalDurationWatched := previousDurationWatched + durationWatched
+
+	// update the DB with the total duration watched
+	s.db.Update(func(tx *bolt.Tx) error {
+		// convert Duration to []byte
+		// c.p. https://stackoverflow.com/a/23004209
+		var buf bytes.Buffer
+		enc := gob.NewEncoder(&buf)
+		err := enc.Encode(totalDurationWatched)
+		if err != nil {
+			return err
+		}
+		b := tx.Bucket([]byte(userWatchedBucket))
+		err = b.Put([]byte(user), []byte(buf.Bytes()))
+		return err
+	})
+
+	log.Println(user, "left the channel, total watched:", fmtDuration(totalDurationWatched))
 }
 
 // helper func to make Durations prettier
@@ -67,40 +160,23 @@ func main() {
 		panic("You must set TWITCH_AUTH_TOKEN")
 	}
 
-	db, err := bolt.Open("tripbot.db", 0600, nil)
-	if err != nil {
-		log.Fatal(err)
+	datastore := NewStore("tripbot.db")
+	if err := datastore.Open(); err != nil {
+		panic(err)
 	}
-	defer db.Close()
-
-	db.Update(func(tx *bolt.Tx) error {
-		_, err := tx.CreateBucket([]byte("user_joins"))
-		if err != nil {
-			log.Panicf("create bucket: %s", err)
-		}
-		return nil
-	})
-
-	db.Update(func(tx *bolt.Tx) error {
-		_, err := tx.CreateBucket([]byte("user_watched"))
-		if err != nil {
-			log.Panicf("create bucket: %s", err)
-		}
-		return nil
-	})
 
 	client := twitch.NewClient(clientUsername, clientAuthenticationToken)
 
 	client.OnUserJoinMessage(func(joinMessage twitch.UserJoinMessage) {
 		if !userIsIgnored(joinMessage.User) {
-			recordUserJoin(joinMessage.User)
+			datastore.recordUserJoin(joinMessage.User)
 			// log.Println(joinMessage.Raw)
 		}
 	})
 
 	client.OnUserPartMessage(func(partMessage twitch.UserPartMessage) {
 		if !userIsIgnored(partMessage.User) {
-			recordUserPart(partMessage.User)
+			datastore.recordUserPart(partMessage.User)
 			// log.Println(partMessage.Raw)
 		}
 	})
@@ -108,7 +184,7 @@ func main() {
 	client.Join(channelToJoin)
 	log.Println("Joined channel", channelToJoin)
 
-	err = client.Connect()
+	err := client.Connect()
 	if err != nil {
 		panic(err)
 	}
