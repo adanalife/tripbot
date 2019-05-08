a5abc46fbce24337211afadba4529a6119790439
Dana Merrick <dana.merrick@postmates.com>
2019-05-08 11:16:37 -0500

    Improvements

--- bin/generate-leaderboard.sh
+++ bin/generate-leaderboard.sh
@@ -1,7 +1,23 @@
 #!/usr/bin/env bash
 
-cp tripbot.db tripbot-copy.db
+echo "starting script"
 
-go run tripbot4000-leaderboard.go > OBS/leaderboard.txt
+# trap ctrl-c and call ctrl_c()
+trap ctrl_c INT
 
+function ctrl_c() {
+	echo "** Caught CTRL-C"
+	exit 1
+}
+
+while true; do
+	if (( RANDOM % 5 == 0 )); then
+		cp tripbot.db tripbot-copy.db
+		go run tripbot4000-leaderboard.go > OBS/leaderboard.txt
+	else
+		# clear out the file
+		echo "" > OBS/leaderboard.txt
+	fi
+	sleep 30
+done
 
