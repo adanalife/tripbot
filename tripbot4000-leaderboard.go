0bb66ca8b9e6cd6e076a6aefa815ebccdb54aff9
Dana Merrick <dana.merrick@postmates.com>
2019-05-08 10:54:53 -0500

    More progress

--- tripbot4000-leaderboard.go
+++ tripbot4000-leaderboard.go
@@ -9,7 +9,16 @@ import (
 	"github.com/boltdb/bolt"
 )
 
-const userWatchedBucket = "user_watched"
+const (
+	userWatchedBucket = "user_watched"
+	userJoinsBucket   = "user_joins"
+)
+
+var ignoredUsers = []string{
+	"adanalife_",
+	"tripbot4000",
+	"nightbot",
+}
 
 func durationToMiles(d time.Duration) int {
 	return int(d.Minutes() / 10)
@@ -26,20 +35,37 @@ func main() {
 
 	db.View(func(tx *bolt.Tx) error {
 		watchedBucket := tx.Bucket([]byte(userWatchedBucket))
+		joinedBucket := tx.Bucket([]byte(userJoinsBucket))
+
 		err := watchedBucket.ForEach(func(k, v []byte) error {
 			user := string(k)
-			if user == "tripbot4000" {
-				return nil
+			// don't include these in leaderboard
+			for _, ignored := range ignoredUsers {
+				if user == ignored {
+					return nil
+				}
 			}
+
+			// fetch the current view duration
+			var joinTime time.Time
+			var currentDuration time.Duration
+			err := joinTime.UnmarshalText(joinedBucket.Get([]byte(user)))
+			if err != nil {
+				currentDuration = 0
+			} else {
+				currentDuration = time.Since(joinTime)
+			}
+
 			duration, err := time.ParseDuration(string(v))
 			if err != nil {
 				return err
 			}
-			intDuration := int(duration)
+			intDuration := int(duration) + int(currentDuration)
 			sortedValues = append(sortedValues, intDuration)
 			reversedMap[intDuration] = user
 			return err
 		})
+
 		return err
 	})
 
