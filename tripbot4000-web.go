274db1543f945a1eb0e0c71dccaff91a0aea2c32
Dana Merrick <dana.merrick@postmates.com>
2019-05-07 22:50:43 -0500

    Adding a simple server

---
+++ tripbot4000-web.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/boltdb/bolt"
+)
+
+const userWatchedBucket = "user_watched"
+
+func durationToMiles(d time.Duration) int {
+	return int(d.Minutes() / 15)
+}
+
+func main() {
+	db, err := bolt.Open("tripbot.db", 0666, &bolt.Options{ReadOnly: true})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	lines := []string{}
+
+	db.View(func(tx *bolt.Tx) error {
+		watchedBucket := tx.Bucket([]byte(userWatchedBucket))
+		err := watchedBucket.ForEach(func(k, v []byte) error {
+			duration, err := time.ParseDuration(string(v))
+			lines = append(lines, fmt.Sprintf("%s has %s miles.\n", k, durationToMiles(duration)))
+			return err
+		})
+		return err
+	})
+
+	http.HandleFunc("/stats", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, strings.Join(lines, ""))
+	})
+
+	http.ListenAndServe(":8088", nil)
+}
