15ccfdf7353b0cfde6092d8db1dde124b4a2207c
Dana Merrick <dana.merrick@postmates.com>
2019-05-12 23:18:28 -0500

    Progress

--- bin/collect-gps.go
+++ bin/collect-gps.go
@@ -1,39 +1,52 @@
 package main
 
 import (
+	"io/ioutil"
 	"log"
-	"path/filepath"
+	_ "path/filepath"
 
-	"github.com/disintegration/imaging"
-	"github.com/otiai10/gosseract"
+	_ "github.com/disintegration/imaging"
+	_ "github.com/otiai10/gosseract"
 )
 
 const (
-	dashcamTimestamp = "2018_1008_005230_049"
-	framePath        = "/Volumes/usbshare1/first frame of every video"
-	croppedPath      = "./cropped"
+	screencapFile = "2018_1008_005230_049.png"
+	screencapPath = "/Volumes/usbshare1/first frame of every video"
+	croppedPath   = "./cropped"
 )
 
 func main() {
-	// imgFile := fmt.Sprintf("%s/%s.png", framePath, dashcamTimestamp)
-	// croppedFile := fmt.Sprintf("cropped-%s.png", dashcamTimestamp)
-	imgFile := filepath.Join(framePath, dashcamTimestamp, ".png")
-	croppedFile := filepath.Join(croppedPath, dashcamTimestamp, ".png")
+	// imgFile := filepath.Join(framePath, screencapFile)
+	// croppedFile := filepath.Join(croppedPath, filepath.Base(screencapFile), ".png")
 
-	// Open a test image.
-	src, err := imaging.Open(imgFile)
+	// _, dashcamTimestamp := filepath.Split(imgFile)
+
+	files, err := ioutil.ReadDir(screencapPath)
 	if err != nil {
-		log.Fatalf("failed to open image: %v", err)
+		log.Fatal(err)
 	}
 
-	// Crop the original image to 300x300px size using the center anchor.
-	src = imaging.CropAnchor(src, 600, 60, imaging.BottomLeft)
-	// Save the resulting image as JPEG.
-	err = imaging.Save(src, croppedFile)
+	for i, f := range files {
+		log.Println(f.Name())
+		if i > 2 {
+			break
+		}
+	}
 
-	client := gosseract.NewClient()
-	defer client.Close()
-	client.SetImage(croppedFile)
-	text, _ := client.Text()
-	log.Println(text)
+	// 	// Open a test image.
+	// 	src, err := imaging.Open(imgFile)
+	// 	if err != nil {
+	// 		log.Fatalf("failed to open image: %v", err)
+	// 	}
+
+	// 	// Crop the original image to 300x300px size using the center anchor.
+	// 	src = imaging.CropAnchor(src, 600, 60, imaging.BottomLeft)
+	// 	// Save the resulting image as JPEG.
+	// 	err = imaging.Save(src, croppedFile)
+
+	// 	client := gosseract.NewClient()
+	// 	defer client.Close()
+	// 	client.SetImage(croppedFile)
+	// 	text, _ := client.Text()
+	// 	log.Println(text)
 }
