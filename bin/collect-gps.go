03970afa6e52db4ac179de74099b2a47610a3ff6
Dana Merrick <dana.merrick@postmates.com>
2019-05-12 23:43:38 -0500

    Rad!

--- bin/collect-gps.go
+++ bin/collect-gps.go
@@ -1,23 +1,39 @@
 package main
 
 import (
-	_ "io/ioutil"
+	"fmt"
 	"log"
 	"os"
+	"path"
 	"path/filepath"
 
 	"github.com/disintegration/imaging"
-	_ "github.com/otiai10/gosseract"
+	"github.com/otiai10/gosseract"
 )
 
 const (
 	screencapFile = "2018_1008_005230_049.png"
 	screencapPath = "/Volumes/usbshare1/first frame of every video"
-	croppedPath   = "./cropped"
+	croppedPath   = "/Volumes/usbshare1/cropped-corners"
 )
 
+func readText(croppedFile string) string {
+	client := gosseract.NewClient()
+	defer client.Close()
+	client.SetImage(croppedFile)
+	text, _ := client.Text()
+	return text
+}
+
 func cropImage(srcFilename string) string {
-	// Open a test image.
+
+	// we want to exit early if the cropped file already exists
+	croppedFile := filepath.Join(croppedPath, path.Base(srcFilename))
+	if fileExists(croppedFile) {
+		return croppedFile
+	}
+
+	// open the image
 	src, err := imaging.Open(srcFilename)
 	if err != nil {
 		log.Fatalf("failed to open image: %v", err)
@@ -26,45 +42,38 @@ func cropImage(srcFilename string) string {
 	// crop the image to just the bottom left text
 	croppedImage := imaging.CropAnchor(src, 600, 60, imaging.BottomLeft)
 	// Save the resulting image
-	croppedFile := filepath.Join(croppedPath, srcFilename)
 	err = imaging.Save(croppedImage, croppedFile)
 	return croppedFile
 }
 
+func fileExists(f string) bool {
+	_, err := os.Stat(f)
+	if os.IsNotExist(err) {
+		return false
+	}
+	return err == nil
+}
+
 func main() {
-	// imgFile := filepath.Join(framePath, screencapFile)
-
-	// _, dashcamTimestamp := filepath.Split(imgFile)
-
-	// files, err := ioutil.ReadDir(screencapPath)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// for i, f := range files {
-	// 	log.Println(f.Name())
-	// 	if i > 2 {
-	// 		break
-	// 	}
-	// }
 
 	err := filepath.Walk(screencapPath,
 		func(path string, info os.FileInfo, err error) error {
 			if err != nil {
 				return err
 			}
-			log.Println("cropping", path)
+			if path == screencapPath {
+				return nil
+			}
+			// log.Println("cropping", path)
+			// log.Println("cropping", path)
+			// log.Println("cropped file:", croppedImage)
 			croppedImage := cropImage(path)
-			log.Println(readText(croppedImage))
-			break
+			textFromImage := readText(croppedImage)
+			fmt.Println(textFromImage)
 			return nil
 		})
 	if err != nil {
 		log.Println(err)
 	}
 
-	// 	client := gosseract.NewClient()
-	// 	defer client.Close()
-	// 	client.SetImage(croppedFile)
-	// 	text, _ := client.Text()
-	// 	log.Println(text)
 }
