383d033c9cfe1b5bdc2026840e129a197a2fecdd
Dana Merrick <dana.merrick@postmates.com>
2019-05-13 19:32:11 -0500

    Renaming file

--- bin/find-broken-vids.sh
+++ bin/find-broken-vids.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
+# this script finds video files that ffmpeg thinks are broken
+
 find -s . -name "2018_*.MP4" -exec bash -c 'echo "{}"; ffmpeg -v error -i "{}" -map 0:1 -f null - >>error.log 2>&1' \;
