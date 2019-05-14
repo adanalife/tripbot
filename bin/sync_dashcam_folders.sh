53bcc053e2cd8bc7e0efbb9dafe29b88d08c4eaf
Dana Merrick <dana.merrick@postmates.com>
2019-05-13 19:43:02 -0500

    Adding a comment

--- bin/sync_dashcam_folders.sh
+++ bin/sync_dashcam_folders.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
+# this script backs up the directory structure to another drive
+
 rsync -madPhi --delete /Volumes/Leeroy/Danas_Photos/Dashcam\ Scratchpad/Dashcam/ /Volumes/usbshare1/Dashcam/
