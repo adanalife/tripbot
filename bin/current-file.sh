50e10e979a1161734daf43d74d609ad6ea7e9314
Dana Merrick <dana.merrick@postmates.com>
2019-05-13 19:30:49 -0500

    Adding a comment

--- bin/current-file.sh
+++ bin/current-file.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
-# lsof +D /Volumes/Leeroy/Danas_Photos/Dashcam_Scratchpad/Dashcam/_all/ | grep MP4 | sed -e 's/^.*2018_/2018_/'
+# this script finds the currently-playing video file
+
 lsof -p $(ps aux | grep -i OBS | grep -v grep | awk '{print $2}') | grep MP4 | sed -e 's/^.*2018_/2018_/'
