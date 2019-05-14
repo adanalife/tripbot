bdc14e2373000349479446c236fdfcd29411e915
Dana Merrick <dana.merrick@postmates.com>
2019-05-13 20:03:52 -0500

    Adding a TODO

--- bin/current-file.sh
+++ bin/current-file.sh
@@ -2,4 +2,6 @@
 
 # this script finds the currently-playing video file
 
+#TODO: error message if more than 1 result from search
+
 lsof -p $(ps aux | grep -i OBS | grep -v grep | awk '{print $2}') | grep MP4 | sed -e 's/^.*2018_/2018_/'
