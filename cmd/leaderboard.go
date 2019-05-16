f86ea2b884712a49a9357bb86ddb5dc40041c8f4
Dana Merrick <dana.merrick@postmates.com>
2019-05-16 16:06:19 -0500

    At least it's progress

--- cmd/leaderboard.go
+++ cmd/leaderboard.go
@@ -25,7 +25,6 @@ func main() {
 
 	fmt.Println("Odometer Leaderboard")
 	for _, user := range userList {
-		// fmt.Println(miles.ForUser(user), "miles:", user)
-		fmt.Println("miles:", user)
+		fmt.Println(miles.ForUser(user), "miles:", user)
 	}
 }
