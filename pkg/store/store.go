3c7ea3061c894093d710c41686d5f5286874df1c
Dana Merrick <dana.merrick@postmates.com>
2019-05-18 01:14:14 -0500

    Moving things around

--- pkg/store/store.go
+++ pkg/store/store.go
@@ -21,7 +21,6 @@ const (
 var currentDatastore *Store
 
 func FindOrCreate() *Store {
-
 	// use the pre-existing datastore if we have one
 	if currentDatastore != nil {
 		return currentDatastore
@@ -46,6 +45,65 @@ func FindOrCreate() *Store {
 	currentDatastore = datastore
 
 	return datastore
+}
+
+type Store struct {
+	path string
+	db   *bolt.DB
+}
+
+func NewStore(path string) *Store {
+	return &Store{
+		path: path,
+	}
+}
+
+// Open opens and initializes the database.
+func (s *Store) Open() error {
+	// Open underlying data store.
+	db, err := bolt.Open(s.path, 0666, &bolt.Options{Timeout: 1 * time.Second})
+	if err != nil {
+		return err
+	}
+	s.db = db
+
+	// Initialize all the required buckets.
+	if err := s.db.Update(func(tx *bolt.Tx) error {
+		tx.CreateBucketIfNotExists([]byte(userJoinsBucket))
+		tx.CreateBucketIfNotExists([]byte(userWatchedBucket))
+		return nil
+	}); err != nil {
+		s.Close()
+		return err
+	}
+
+	return nil
+}
+
+func (s *Store) Close() error {
+	if s.db != nil {
+		var onlineUsers = []string{}
+
+		// first we make a list of all of the online users
+		s.db.View(func(tx *bolt.Tx) error {
+			joinedBucket := tx.Bucket([]byte(userJoinsBucket))
+			err := joinedBucket.ForEach(func(k, _ []byte) error {
+				user := string(k)
+				onlineUsers = append(onlineUsers, user)
+				return nil
+			})
+			return err
+		})
+
+		// then we loop over it and record the current watched duration
+		for _, user := range onlineUsers {
+			log.Println("logging out", user)
+			s.RecordUserPart(string(user))
+		}
+
+		s.db.Close()
+	}
+	return nil
 }
 
 func (s *Store) MilesForUser(user string) int {
@@ -130,65 +188,6 @@ func (s *Store) DurationForUser(user string) time.Duration {
 		return err
 	})
 	return *previousDurationWatched
-}
-
-type Store struct {
-	path string
-	db   *bolt.DB
-}
-
-func NewStore(path string) *Store {
-	return &Store{
-		path: path,
-	}
-}
-
-// Open opens and initializes the database.
-func (s *Store) Open() error {
-	// Open underlying data store.
-	db, err := bolt.Open(s.path, 0666, &bolt.Options{Timeout: 1 * time.Second})
-	if err != nil {
-		return err
-	}
-	s.db = db
-
-	// Initialize all the required buckets.
-	if err := s.db.Update(func(tx *bolt.Tx) error {
-		tx.CreateBucketIfNotExists([]byte(userJoinsBucket))
-		tx.CreateBucketIfNotExists([]byte(userWatchedBucket))
-		return nil
-	}); err != nil {
-		s.Close()
-		return err
-	}
-
-	return nil
-}
-
-func (s *Store) Close() error {
-	if s.db != nil {
-		var onlineUsers = []string{}
-
-		// first we make a list of all of the online users
-		s.db.View(func(tx *bolt.Tx) error {
-			joinedBucket := tx.Bucket([]byte(userJoinsBucket))
-			err := joinedBucket.ForEach(func(k, _ []byte) error {
-				user := string(k)
-				onlineUsers = append(onlineUsers, user)
-				return nil
-			})
-			return err
-		})
-
-		// then we loop over it and record the current watched duration
-		for _, user := range onlineUsers {
-			log.Println("logging out", user)
-			s.RecordUserPart(string(user))
-		}
-
-		s.db.Close()
-	}
-	return nil
 }
 
 func (s *Store) PrintStats() {
