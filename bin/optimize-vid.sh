a1381e3f48bb68a875e1837d3d7686fb57ca9a5c
Dana Merrick <dana.merrick@postmates.com>
2019-05-22 20:37:05 -0500

    Optimizing

--- bin/optimize-vid.sh
+++ bin/optimize-vid.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 # this script takes a video file and generates an optimized-for-twitch version
-#TODO: finish me!
 
 # OBS outout from VLC plugin:
 # info: [x264 encoder: 'simple_h264_stream'] preset: faster
@@ -21,12 +20,7 @@ file=$1
 no_extension=${file%.MP4}
 just_file=${no_extension##*/}
 
-#ffmpeg -i $1 -preset slow -crf 21 -bufsize 10000k -maxrate 5000k -pix_fmt yuv420p -c:v libx264 -x264opts keyint=2:no-scenecut -s 1920x1080 -r 60 -an ${just_file}_opt.MP4
-# ffmpeg -i $1 -preset veryslow -crf 15 -bufsize 20000k -maxrate 10000k -pix_fmt yuv420p -c:v libx264 -x264opts keyint=2:no-scenecut -s 1920x1080 -r 60 -an ${just_file}_opt.MP4
-ffmpeg -i $1 -preset faster -crf 0 -bufsize 20000k -maxrate 10000k -pix_fmt yuv420p -c:v libx264 -x264opts keyint=120:no-scenecut -s 1920x1080 -r 60 -an ${just_file}_opt.MP4
+# for info on keyint=120:no-scenecut
+# https://video.stackexchange.com/a/24684
+ffmpeg -i $1 -preset faster -crf 10 -bufsize 20000k -maxrate 10000k -pix_fmt yuv420p -c:v libx264 -x264opts keyint=120:no-scenecut -s 1920x1080 -r 60 -an ${just_file}_opt.MP4
 
-
-# -preset slow
-# -profile:v <profile>
-# -sws_flags <scaler algorithm>
-# -hls_list_size <number of playlist entries>
