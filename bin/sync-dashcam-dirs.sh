9dcf2d0be1ab79eb7f32876b7c745471e64ed77f
Dana Merrick <dana.merrick@postmates.com>
2019-05-22 21:34:51 -0500

    small tweak

--- bin/sync-dashcam-dirs.sh
+++ bin/sync-dashcam-dirs.sh
@@ -3,3 +3,4 @@
 # this script backs up the directory structure to another drive
 
 rsync -madPhi --delete /Volumes/Leeroy/Danas_Photos/Dashcam\ Scratchpad/Dashcam/ /Volumes/usbshare1/Dashcam/
+# rsync -n -madPhi --delete /Volumes/usbshare1/Dashcam/ /Volumes/Leeroy/Danas_Photos/Dashcam\ Scratchpad/Dashcam/
