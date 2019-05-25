27826cda13639323630348546c209dffc1f5eca2
Dana Merrick <dana.merrick@postmates.com>
2019-05-25 15:37:56 -0500

    Improvements

--- bin/swap-optimized-vids.sh
+++ bin/swap-optimized-vids.sh
@@ -17,15 +17,18 @@ ALL_DIR="${DASHCAM_DIR}/_all"
 echo $OPTIMIZED_DIR
 echo $UNOPTIMIZED_DIR
 
-for file in ${ALL_DIR}/*.MP4; do
+
+for file in ${ALL_DIR}/*; do
+        # remove everything up to the slash and the extension
 	slug="$(echo $file | sed -e 's!^.*/!!' -e 's/\.MP4//')"
 	optimized="${OPTIMIZED_DIR}/${slug}_opt.MP4"
+
 	if [ -e $optimized ]; then
-		echo "optimized version exists: $optimized"
+		echo "optimized version exists, swapping $slug"
 		# echo "mv $file ${UNOPTIMIZED_DIR}/"
 		# echo "mv $optimized ${ALL_DIR}/"
 	else
-		echo "no optimized candidate for $file"
+		echo "no optimized candidate for $slug, moving on"
 	fi
 done
 
