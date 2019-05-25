5781edfba4bff5909c66773fe80e3016a70cc605
Dana Merrick <dana.merrick@postmates.com>
2019-05-25 11:02:00 -0500

    This is much better now

--- bin/print-directory-structure.sh
+++ bin/print-directory-structure.sh
@@ -3,8 +3,21 @@
 # this script recursively lists all files in the current dir
 # it is used to back up the current directory structure
 
-# https://stackoverflow.com/questions/1767384/ls-command-how-can-i-get-a-recursive-full-path-listing-one-line-per-file
+if [[ $# -eq 0 ]] ; then
+    echo "USAGE: $0 dashcam-dir"
+    exit 0
+fi
+
+DASHCAM_DIR=$1
+
+# we cd into the dir so we don't include the dir name in output
+cd $DASHCAM_DIR
+
+# https://stackoverflow.com/questions/1767384
 ls -R . | awk '
 /:$/&&f{s=$0;f=0}
 /:$/&&!f{sub(/:$/,"");s=$0;f=1;next}
 NF&&f{ print s"/"$0 }'
+
+# go back to where we started
+cd -
