23562d7896dcf57e02f25eb3662fe243a632a093
Dana Merrick <dana.merrick@postmates.com>
2019-05-25 17:00:04 -0500

    Adding a TODO

--- bin/start-obs.sh
+++ bin/start-obs.sh
@@ -3,6 +3,7 @@
 PID_FILE=OBS/OBS.pid
 
 echo "starting OBS..."
+#TODO: should be --startstreaming
 nice -n "-15" /Applications/OBS.app/Contents/MacOS/OBS -start >> log/obs-$(date "+%Y-%m-%d").log 2>&1 &
 
 # save pid to file
