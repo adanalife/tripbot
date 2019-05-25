426fb11a3ea546e5956c664711b07d08440760a4
Dana Merrick <dana.merrick@postmates.com>
2019-05-25 15:37:48 -0500

    Adding a trap

--- bin/optimize-all.sh
+++ bin/optimize-all.sh
@@ -8,6 +8,14 @@ fi
 DASHCAM_DIR=$1
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 
+# trap ctrl-c and call ctrl_c()
+trap ctrl_c INT
+
+function ctrl_c() {
+  echo "** Caught CTRL-C"
+  exit 1
+}
+
 cd "${DASHCAM_DIR}/optimized"
 
 for file in ${DASHCAM_DIR}/_all/*.MP4; do
