087f13ef5b527a9ecf892a0b77c48fa061fccd2d
Dana Merrick <dana.merrick@postmates.com>
2019-05-26 17:53:05 -0500

    Cleaning up

--- pkg/miles/miles.go
+++ pkg/miles/miles.go
@@ -10,6 +10,7 @@ import (
 	"github.com/dmerrick/danalol-stream/pkg/helpers"
 )
 
+// ForUser returns the miles for a given user
 func ForUser(user string) int {
 	evnts := []events.Event{}
 	query := fmt.Sprintf("SELECT username, event, date_created from events where username = '%s' AND event in ('login', 'logout')", user)
@@ -22,6 +23,7 @@ func ForUser(user string) int {
 	return helpers.DurationToMiles(dur)
 }
 
+// splitIntoPairs takes a list of events and smartly pairs together matching login/logout events
 func splitIntoPairs(evnts []events.Event) [][]events.Event {
 	var pairs [][]events.Event
 	// no events were found
@@ -31,18 +33,19 @@ func splitIntoPairs(evnts []events.Event) [][]events.Event {
 
 	// check if their most recent event is a login
 	if evnts[len(evnts)-1].Event == "login" {
-		log.Println("user is logged in, adding a logout event")
+		// log.Println("user is logged in, adding a logout event")
 		// ... in which case add the current time to the list
 		evnts = append(evnts, events.Event{DateCreated: time.Now(), Event: "logout"})
 	}
 
+	// now we're going to loop over all of the events and split them into pairs
 	for i := 0; i < len(evnts)-1; i++ {
 		// we're only looking for logins here
 		if evnts[i].Event == "logout" {
 			continue
 		}
 
-		// this will contain a login/logout pair
+		// this will be our login/logout pair
 		pair := make([]events.Event, 2)
 
 		// check if the _next_ event is a login
@@ -51,22 +54,20 @@ func splitIntoPairs(evnts []events.Event) [][]events.Event {
 			continue
 		}
 
-		// okay so we know the next event isn't a login
+		// okay so we know the next event isn't a login, we know the next is a logout
 		if evnts[i].Event == "login" {
 			// set the pair
 			pair[0] = evnts[i]
 			pair[1] = evnts[i+1]
 		}
 
-		if len(pair) != 2 {
-			log.Fatal("pair wasn't full for some reason")
-		}
-
+		// add this login/logout pair to the list
 		pairs = append(pairs, pair)
 	}
 	return pairs
 }
 
+// combinePairs takes pairs of login/logout events and sums up the time between them
 func combinePairs(pairs [][]events.Event) time.Duration {
 	var durSum time.Duration
 	for _, pair := range pairs {
