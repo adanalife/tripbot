6d4643ed129d2185f9c3cfbd248cb3d321c3613b
Dana Merrick <dana.merrick@postmates.com>
2019-05-29 20:23:03 -0500

    Moving things around

--- pkg/tripbot/tripbot.go
+++ pkg/tripbot/tripbot.go
@@ -31,83 +31,6 @@ var uptime time.Time
 // randomized so it starts with a new one every restart
 var helpIndex = rand.Intn(len(config.HelpMessages))
 
-func Initialize() *twitch.Client {
-	var err error
-	uptime = time.Now()
-
-	// load ENV vars from .env file
-	err = godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
-
-	// first we must check for required ENV vars
-	if os.Getenv("DASHCAM_DIR") == "" {
-		panic("You must set DASHCAM_DIR")
-	}
-	botUsername = os.Getenv("BOT_USERNAME")
-	if botUsername == "" {
-		panic("You must set BOT_USERNAME")
-	}
-	clientAuthenticationToken = os.Getenv("TWITCH_AUTH_TOKEN")
-	if clientAuthenticationToken == "" {
-		panic("You must set TWITCH_AUTH_TOKEN")
-	}
-	twitchClientID = os.Getenv("TWITCH_CLIENT_ID")
-	if twitchClientID == "" {
-		panic("You must set TWITCH_CLIENT_ID")
-	}
-	googleMapsAPIKey = os.Getenv("GOOGLE_MAPS_API_KEY")
-	if googleMapsAPIKey == "" {
-		panic("You must set GOOGLE_MAPS_API_KEY")
-	}
-
-	// set up geocoder (for translating coords to places)
-	geocoder.ApiKey = googleMapsAPIKey
-
-	// initialize the twitch API client
-	//TODO: rename me to Initialize()
-	_, err = mytwitch.FindOrCreateClient(twitchClientID)
-	if err != nil {
-		log.Fatal("unable to create twitch API client", err)
-	}
-
-	// initialize the SQL database
-	database.DBCon, err = database.Initialize()
-	if err != nil {
-		log.Fatal("error initializing the DB", err)
-	}
-	defer database.DBCon.Close()
-
-	// initialize the local datastore
-	datastore = store.FindOrCreate(config.DBPath)
-
-	return twitch.NewClient(botUsername, clientAuthenticationToken)
-}
-
-func UserJoin(joinMessage twitch.UserJoinMessage) {
-	events.LoginIfNecessary(joinMessage.User)
-}
-
-func UserPart(partMessage twitch.UserPartMessage) {
-	events.LogoutIfNecessary(partMessage.User)
-}
-
-func UserNotice(message twitch.UserNoticeMessage) {
-	log.Println("user notice:", message.SystemMsg, "***", message.Emotes, "***", message.Tags)
-	// send message to chat if someone subs
-	msg := fmt.Sprintf("%s Your support powers me bleedPurple", message.Message)
-	client.Say(config.ChannelName, msg)
-}
-
-func Whisper(message twitch.WhisperMessage) {
-	log.Println("whisper from", message.User.Name, ":", message.Message)
-	// if the message comes from me, then post the message to chat
-	if message.User.Name == config.ChannelName {
-		client.Say(config.ChannelName, message.Message)
-	}
-}
-
 // all chat messages
 func PrivateMessage(message twitch.PrivateMessage) {
 	user := message.User.Name
@@ -317,19 +240,79 @@ func PrivateMessage(message twitch.PrivateMessage) {
 	}
 }
 
-// func main() {
+func UserJoin(joinMessage twitch.UserJoinMessage) {
+	events.LoginIfNecessary(joinMessage.User)
+}
 
-// 	err := godotenv.Load()
-// 	if err != nil {
-// 		log.Fatal("Error loading .env file")
-// 	}
+func UserPart(partMessage twitch.UserPartMessage) {
+	events.LogoutIfNecessary(partMessage.User)
+}
 
-// 	// catch CTRL-C and clean up
-// 	c := make(chan os.Signal)
-// 	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
-// 	go func() {
-// 		<-c
-// 		log.Println("caught CTRL-C")
-// 		events.LogoutAll(uptime)
-// 		os.Exit(1)
-// 	}()
+func UserNotice(message twitch.UserNoticeMessage) {
+	log.Println("user notice:", message.SystemMsg, "***", message.Emotes, "***", message.Tags)
+	// send message to chat if someone subs
+	msg := fmt.Sprintf("%s Your support powers me bleedPurple", message.Message)
+	client.Say(config.ChannelName, msg)
+}
+
+func Whisper(message twitch.WhisperMessage) {
+	log.Println("whisper from", message.User.Name, ":", message.Message)
+	// if the message comes from me, then post the message to chat
+	if message.User.Name == config.ChannelName {
+		client.Say(config.ChannelName, message.Message)
+	}
+}
+
+func Initialize() *twitch.Client {
+	var err error
+	uptime = time.Now()
+
+	// load ENV vars from .env file
+	err = godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	// first we must check for required ENV vars
+	if os.Getenv("DASHCAM_DIR") == "" {
+		panic("You must set DASHCAM_DIR")
+	}
+	botUsername = os.Getenv("BOT_USERNAME")
+	if botUsername == "" {
+		panic("You must set BOT_USERNAME")
+	}
+	clientAuthenticationToken = os.Getenv("TWITCH_AUTH_TOKEN")
+	if clientAuthenticationToken == "" {
+		panic("You must set TWITCH_AUTH_TOKEN")
+	}
+	twitchClientID = os.Getenv("TWITCH_CLIENT_ID")
+	if twitchClientID == "" {
+		panic("You must set TWITCH_CLIENT_ID")
+	}
+	googleMapsAPIKey = os.Getenv("GOOGLE_MAPS_API_KEY")
+	if googleMapsAPIKey == "" {
+		panic("You must set GOOGLE_MAPS_API_KEY")
+	}
+
+	// set up geocoder (for translating coords to places)
+	geocoder.ApiKey = googleMapsAPIKey
+
+	// initialize the twitch API client
+	//TODO: rename me to Initialize()
+	_, err = mytwitch.FindOrCreateClient(twitchClientID)
+	if err != nil {
+		log.Fatal("unable to create twitch API client", err)
+	}
+
+	// initialize the SQL database
+	database.DBCon, err = database.Initialize()
+	if err != nil {
+		log.Fatal("error initializing the DB", err)
+	}
+	defer database.DBCon.Close()
+
+	// initialize the local datastore
+	datastore = store.FindOrCreate(config.DBPath)
+
+	return twitch.NewClient(botUsername, clientAuthenticationToken)
+}
