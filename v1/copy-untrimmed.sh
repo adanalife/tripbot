fb04aae1941bd8482f356f205aaf09da70d5b2ff
Dana Merrick <dana.merrick@postmates.com>
2019-05-31 22:33:22 -0500

    Adding sister helper

--- v1/copy-untrimmed.sh
+++ v1/copy-untrimmed.sh
@@ -7,3 +7,5 @@ mkdir n
 for file in $(ls _all | grep -B1 _b | awk -F '\n' 'ln ~ /^$/ { ln = "matched"; print $1 } $1 ~ /^--$/ { ln = "" }' | grep -v _a | grep -v _c); do
 echo "mv _all/$file n/"
 done
+
+#ls _all/ | grep -A1 _a | grep -v '\-\-' | grep -v '_a' | grep -v _b
