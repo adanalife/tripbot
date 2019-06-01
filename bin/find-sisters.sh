5eed679a00512179581d0c94d6ecd848619ad39b
Dana Merrick <dana.merrick@postmates.com>
2019-06-01 00:03:38 -0500

    Improvements

--- bin/find-sisters.sh
+++ bin/find-sisters.sh
@@ -3,6 +3,14 @@
 # this script finds videos that have been clipped (i.e. have an _a or _b suffix),
 # and lists their "sister" files (the adjacent file that may also need to be clipped)
 
+if [[ $# -eq 0 ]] ; then
+  echo "USAGE: $0 dashcam-dir"
+  exit 0
+fi
+DASHCAM_DIR=$1
+
+cd $DASHCAM_DIR
+
 # mkdir tmp
 
 # https://stackoverflow.com/a/6282057
@@ -14,3 +22,5 @@
 
 ls _all | grep -B1 _b | awk -F '\n' 'ln ~ /^$/ { ln = "matched"; print $1 } $1 ~ /^--$/ { ln = "" }' | grep -v _a | grep -v _c
 ls _all | grep -A1 _a | grep -v '\-\-' | grep -v '_a' | grep -v _b
+
+cd - > /dev/null
