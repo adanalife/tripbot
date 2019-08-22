946089c309a0bdc807b75c47f8a4bb7b761a2846
Dana Merrick <dana.merrick@postmates.com>
2019-08-21 21:14:29 -0500

    Splitting in TWAIN

--- pkg/video/video.go
+++ pkg/video/video.go
@@ -2,110 +2,17 @@ package video
 
 import (
 	"errors"
-	"fmt"
 	"log"
 	"os/exec"
 	"path"
-	"regexp"
-	"strconv"
 	"strings"
-	"time"
 
-	"github.com/dmerrick/danalol-stream/pkg/config"
 	"github.com/dmerrick/danalol-stream/pkg/helpers"
 	"github.com/dmerrick/danalol-stream/pkg/ocr"
 )
 
 // these are used to keep track of the current video
 var CurrentlyPlaying, previouslyPlaying string
-
-// these are different timestamps we have screenshots prepared for
-// the "000" corresponds to 0m0s, "130" corresponds to 1m30s
-var timestampsToTry = []string{
-	"000",
-	"015",
-	"030",
-	"045",
-	"100",
-	"115",
-	"130",
-	"145",
-	"200",
-	"215",
-	"230",
-	"245",
-}
-
-// Videos represent a video file containing dashcam footage
-type Video struct {
-	//TODO can Slug be private?
-	Slug string
-}
-
-func New(file string) (Video, error) {
-	var newVid Video
-
-	if file == "" {
-		return newVid, errors.New("no file provided")
-	}
-	fileName := path.Base(file)
-	dashStr := removeFileExtension(fileName)
-
-	// validate the dash string
-	err := validate(dashStr)
-	if err != nil {
-		return newVid, err
-	}
-	newVid = Video{dashStr}
-	return newVid, err
-}
-
-// ex: 2018_0514_224801_013_a_opt
-func (v Video) String() string {
-	return v.Slug
-}
-
-// a DashStr is the string we get from the dashcam
-// an example file: 2018_0514_224801_013.MP4
-// an example dashstr: 2018_0514_224801_013
-// ex: 2018_0514_224801_013
-func (v Video) DashStr() string {
-	//TODO: this never should have happened, but it did and it crashed the bot
-	if len(v.Slug) < 20 {
-		return ""
-	}
-	return v.Slug[:20]
-}
-
-// ex: 2018_0514_224801_013.MP4
-func (v Video) File() string {
-	return fmt.Sprintf("%s.MP4", v.Slug)
-}
-
-// ex: /Volumes/.../2018_0514_224801_013.MP4
-func (v Video) Path() string {
-	return path.Join(config.VideoDir(), v.File())
-}
-
-// Date returns a time.Time object for the video
-func (v Video) Date() time.Time {
-	vidStr := v.String()
-	year, _ := strconv.Atoi(vidStr[:4])
-	month, _ := strconv.Atoi(vidStr[5:7])
-	day, _ := strconv.Atoi(vidStr[7:9])
-	hour, _ := strconv.Atoi(vidStr[10:12])
-	minute, _ := strconv.Atoi(vidStr[12:14])
-	second, _ := strconv.Atoi(vidStr[14:16])
-
-	t := time.Date(year, time.Month(month), day, hour, minute, second, 0, time.UTC)
-	return t
-}
-
-// timestamp is something like 000, 030, 100, etc
-func (v Video) screencap(timestamp string) string {
-	screencapFile := fmt.Sprintf("%s-%s.png", v.DashStr(), timestamp)
-	return path.Join(config.ScreencapDir(), timestamp, screencapFile)
-}
 
 func (v Video) CoordsWithRetry() (float64, float64, error) {
 	for _, timestamp := range timestampsToTry {
@@ -132,27 +39,4 @@ func GetCurrentlyPlaying() {
 	} else {
 		// it's a new video, reset the timer
 	}
-}
-
-func validate(dashStr string) error {
-	if len(dashStr) < 20 {
-		return errors.New("dash string too short")
-	}
-	shortened := dashStr[:20]
-
-	if strings.HasPrefix(".", shortened) {
-		return errors.New("dash string can't be a hidden file")
-	}
-
-	//TODO: this should probably live in an init()
-	var validDashStr = regexp.MustCompile(`^[_0-9]{20}$`)
-	if !validDashStr.MatchString(shortened) {
-		return errors.New("dash string did not match regex")
-	}
-	return nil
-}
-
-func removeFileExtension(filename string) string {
-	ext := path.Ext(filename)
-	return filename[0 : len(filename)-len(ext)]
 }
