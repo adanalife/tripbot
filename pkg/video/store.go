1148c3bd2c2029e93dc9f8dbd7b90f49ae92de61
Dana Merrick <dana.merrick@postmates.com>
2019-08-22 17:41:05 -0500

    Splitting into two files

---
+++ pkg/video/store.go
@@ -0,0 +1,128 @@
+package video
+
+import (
+	"errors"
+	"fmt"
+	"log"
+	"regexp"
+	"strings"
+	"time"
+
+	"github.com/dmerrick/danalol-stream/pkg/database"
+)
+
+// LoadOrCreate() will look up the video in the DB,
+// or add it to the DB if it's not there yet
+func LoadOrCreate(path string) (Video, error) {
+	slug := slug(path)
+
+	vid, err := load(slug)
+	if err != nil {
+		// create a new video
+		vid, err = create(slug)
+	}
+
+	return vid, err
+}
+
+// Location returns a lat/lng pair
+func load(slug string) (Video, error) {
+	var newVid Video
+	// try to find the slug in the DB
+	videos := []Video{}
+	query := fmt.Sprintf("SELECT * FROM videos WHERE slug='%s'", slug)
+	err := database.DBCon.Select(&videos, query)
+	if err != nil {
+		log.Println("error fetching vid from DB:", err)
+		return newVid, err
+	}
+
+	// did we find anything in the DB?
+	if len(videos) == 0 {
+		err = errors.New("no matches found")
+		return newVid, err
+	}
+	return videos[0], nil
+}
+
+// create will create a new Video from a slug
+//TODO: this is kinda weird, we create an empty Video
+// and then we save it to the DB... maybe we could just
+// save right to the DB? It would take some refactoring.
+func create(file string) (Video, error) {
+	var newVid Video
+	var blankDate time.Time
+
+	if file == "" {
+		return newVid, errors.New("no file provided")
+	}
+	slug := slug(file)
+
+	// validate the dash string
+	err := validate(slug)
+	if err != nil {
+		return newVid, err
+	}
+
+	// create new (mostly) empty vid
+	newVid = Video{
+		Slug:        slug,
+		Lat:         0,
+		Lng:         0,
+		Flagged:     false,
+		DateFilmed:  blankDate,
+		DateCreated: blankDate,
+	}
+
+	// store the video in the DB
+	err = newVid.save()
+	if err != nil {
+		log.Println("error saving to DB:", err)
+	}
+
+	// now fetch it from the DB
+	//TODO: this is an extra DB call, do we care?
+	dbVid, err := load(slug)
+
+	return dbVid, err
+}
+
+// save() will store the video in the DB
+func (v Video) save() error {
+	flagged := false
+	// try to get at least one good coords pair
+	lat, lng, err := v.ocrCoords()
+	if err != nil {
+		log.Println("error fetching coords:", err)
+		flagged = true
+	}
+
+	tx := database.DBCon.MustBegin()
+	tx.MustExec(
+		"INSERT INTO videos (slug, lat, lng, date_filmed, flagged) VALUES ($1, $2, $3, $4, $5)",
+		v.Slug,
+		lat,
+		lng,
+		v.toDate(),
+		flagged,
+	)
+	return tx.Commit()
+}
+
+func validate(dashStr string) error {
+	if len(dashStr) < 20 {
+		return errors.New("dash string too short")
+	}
+	shortened := dashStr[:20]
+
+	if strings.HasPrefix(".", shortened) {
+		return errors.New("dash string can't be a hidden file")
+	}
+
+	//TODO: this should probably live in an init()
+	var validDashStr = regexp.MustCompile(`^[_0-9]{20}$`)
+	if !validDashStr.MatchString(shortened) {
+		return errors.New("dash string did not match regex")
+	}
+	return nil
+}
--- pkg/video/type.go
+++ pkg/video/type.go
@@ -4,15 +4,11 @@ import (
 	"database/sql"
 	"errors"
 	"fmt"
-	"log"
 	"path"
-	"regexp"
 	"strconv"
-	"strings"
 	"time"
 
 	"github.com/dmerrick/danalol-stream/pkg/config"
-	"github.com/dmerrick/danalol-stream/pkg/database"
 	"github.com/dmerrick/danalol-stream/pkg/ocr"
 )
 
@@ -27,20 +23,6 @@ type Video struct {
 	Flagged     bool          `db:"flagged"`
 	DateFilmed  time.Time     `db:"date_filmed"`
 	DateCreated time.Time     `db:"date_created"`
-}
-
-// LoadOrCreate() will look up the video in the DB,
-// or add it to the DB if it's not there yet
-func LoadOrCreate(path string) (Video, error) {
-	slug := slug(path)
-
-	vid, err := load(slug)
-	if err != nil {
-		// create a new video
-		vid, err = create(slug)
-	}
-
-	return vid, err
 }
 
 // Location returns a lat/lng pair
@@ -105,89 +87,6 @@ func (v Video) ocrCoords() (float64, float64, error) {
 	return 0, 0, errors.New("none of the screencaps had valid coords")
 }
 
-func load(slug string) (Video, error) {
-	var newVid Video
-	// try to find the slug in the DB
-	videos := []Video{}
-	query := fmt.Sprintf("SELECT * FROM videos WHERE slug='%s'", slug)
-	err := database.DBCon.Select(&videos, query)
-	if err != nil {
-		log.Println("error fetching vid from DB:", err)
-		return newVid, err
-	}
-
-	// did we find anything in the DB?
-	if len(videos) == 0 {
-		err = errors.New("no matches found")
-		return newVid, err
-	}
-	return videos[0], nil
-}
-
-// create will create a new Video from a slug
-//TODO: this is kinda weird, we create an empty Video
-// and then we save it to the DB... maybe we could just
-// save right to the DB? It would take some refactoring.
-func create(file string) (Video, error) {
-	var newVid Video
-	var blankDate time.Time
-
-	if file == "" {
-		return newVid, errors.New("no file provided")
-	}
-	slug := slug(file)
-
-	// validate the dash string
-	err := validate(slug)
-	if err != nil {
-		return newVid, err
-	}
-
-	// create new (mostly) empty vid
-	newVid = Video{
-		Slug:        slug,
-		Lat:         0,
-		Lng:         0,
-		Flagged:     false,
-		DateFilmed:  blankDate,
-		DateCreated: blankDate,
-	}
-
-	// store the video in the DB
-	err = newVid.save()
-	if err != nil {
-		log.Println("error saving to DB:", err)
-	}
-
-	// now fetch it from the DB
-	//TODO: this is an extra DB call, do we care?
-	dbVid, err := load(slug)
-
-	return dbVid, err
-}
-
-// save() will store the video in the DB
-func (v Video) save() error {
-	flagged := false
-	// try to get at least one good coords pair
-	lat, lng, err := v.ocrCoords()
-	if err != nil {
-		log.Println("error fetching coords:", err)
-		flagged = true
-	}
-
-	tx := database.DBCon.MustBegin()
-	tx.MustExec(
-		"INSERT INTO videos (slug, lat, lng, date_filmed, flagged) VALUES ($1, $2, $3, $4, $5)",
-		v.Slug,
-		lat,
-		lng,
-		v.toDate(),
-		flagged,
-	)
-	return tx.Commit()
-}
-
 // slug strips the path and extension off the file
 func slug(file string) string {
 	fileName := path.Base(file)
@@ -215,24 +114,6 @@ var timestampsToTry = []string{
 func (v Video) screencap(timestamp string) string {
 	screencapFile := fmt.Sprintf("%s-%s.png", v.DashStr(), timestamp)
 	return path.Join(config.ScreencapDir(), timestamp, screencapFile)
-}
-
-func validate(dashStr string) error {
-	if len(dashStr) < 20 {
-		return errors.New("dash string too short")
-	}
-	shortened := dashStr[:20]
-
-	if strings.HasPrefix(".", shortened) {
-		return errors.New("dash string can't be a hidden file")
-	}
-
-	//TODO: this should probably live in an init()
-	var validDashStr = regexp.MustCompile(`^[_0-9]{20}$`)
-	if !validDashStr.MatchString(shortened) {
-		return errors.New("dash string did not match regex")
-	}
-	return nil
 }
 
 func removeFileExtension(filename string) string {
