01f8b38f4c1596e852fee0238c6d456354a06be4
Dana Merrick <dana.merrick@postmates.com>
2019-08-21 21:20:54 -0500

    Refactoring

--- pkg/video/video.go
+++ pkg/video/video.go
@@ -14,6 +14,21 @@ import (
 // these are used to keep track of the current video
 var CurrentlyPlaying, previouslyPlaying string
 
+// GetCurrentlyPlaying will use lsof to figure out
+// which dashcam video is currently playing (seriously)
+func GetCurrentlyPlaying() {
+	// save the video we used last time
+	previouslyPlaying = CurrentlyPlaying
+	// figure out whats currently playing
+	CurrentlyPlaying = figureOutCurrentVideo()
+
+	if CurrentlyPlaying == previouslyPlaying {
+		// it's the same video as last time
+	} else {
+		// it's a new video, reset the timer
+	}
+}
+
 func (v Video) CoordsWithRetry() (float64, float64, error) {
 	for _, timestamp := range timestampsToTry {
 		lat, lon, err := ocr.CoordsFromImage(v.screencap(timestamp))
@@ -24,19 +39,14 @@ func (v Video) CoordsWithRetry() (float64, float64, error) {
 	return 0, 0, errors.New("none of the screencaps had valid coords")
 }
 
-func GetCurrentlyPlaying() {
+func figureOutCurrentVideo() string {
 	// run the shell script to get currently-playing video
 	scriptPath := path.Join(helpers.ProjectRoot(), "bin/current-file.sh")
 	// cmd := fmt.Sprintf("/usr/bin/cd %s && %s", helpers.ProjectRoot(), scriptPath)
 	out, err := exec.Command(scriptPath).Output()
 	if err != nil {
 		log.Printf("failed to run script: %v", err)
+		return ""
 	}
-	previouslyPlaying = CurrentlyPlaying
-	CurrentlyPlaying = strings.TrimSpace(string(out))
-	if CurrentlyPlaying == previouslyPlaying {
-		// it's the same video as last time
-	} else {
-		// it's a new video, reset the timer
-	}
+	return strings.TrimSpace(string(out))
 }
