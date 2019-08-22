d5982c95f2fb13ecb46edb35c8074fbed3ec4bac
Dana Merrick <dana.merrick@postmates.com>
2019-08-22 18:29:03 -0500

    Adding tricks

--- bin/current-file.sh
+++ bin/current-file.sh
@@ -7,5 +7,6 @@
 # SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 # lsof -p $(cat ${SCRIPTPATH}/../OBS/OBS.pid) | grep -i '\.MP4' | sed -e 's/^.*2018_/2018_/'
 
+# echo "2018_0522_201919_010_opt.MP4"
+# ssh void -- "cd other_projects/danalol-stream && /Users/dmerrick/other_projects/danalol-stream/bin/current-file.sh"
 lsof -p $(cat OBS/OBS.pid) | grep -i '\.MP4' | sed -e 's/^.*2018_/2018_/'
-
