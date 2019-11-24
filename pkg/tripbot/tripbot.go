b5f46b57a13a2cbf0ad7a112d4616db57b5cd0b7
Dana Merrick <dana.merrick@postmates.com>
2019-11-23 18:36:47 -0600

    Update sub list

--- pkg/tripbot/tripbot.go
+++ pkg/tripbot/tripbot.go
@@ -181,6 +181,9 @@ func UserPart(partMessage twitch.UserPartMessage) {
 
 // send message to chat if someone subs
 func UserNotice(message twitch.UserNoticeMessage) {
+	// update the internal subscriber list
+	mytwitch.GetSubscribers()
+
 	if message.Message != "" {
 		msg := fmt.Sprintf("%s just subscribed with message: %s", message.User.DisplayName, message.Message)
 		client.Say(config.ChannelName, msg)
@@ -188,8 +191,7 @@ func UserNotice(message twitch.UserNoticeMessage) {
 		msg := fmt.Sprintf("%s just subscribed", message.User.DisplayName)
 		client.Say(config.ChannelName, msg)
 	}
-	msg := fmt.Sprintf("Thank you. Your support keeps me running bleedPurple")
-	client.Say(config.ChannelName, msg)
+	client.Say(config.ChannelName, "Thank you. Your support keeps me running bleedPurple")
 }
 
 // if the message comes from me, then post the message to chat
