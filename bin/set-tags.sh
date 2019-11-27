cbf75d7af3961f9285c24fe4e2586a9d1ca918e2
Dana Merrick <dana.merrick@postmates.com>
2019-11-27 13:33:54 -0600

    Updating scripts

--- bin/set-tags.sh
+++ bin/set-tags.sh
@@ -1,23 +1,5 @@
 #!/usr/bin/env bash
 
-source .env
-# CHANNEL_NAME=adanalife_
-
-# get the broadcaster ID
-# CHANNEL_ID=$(curl -s -H "Client-ID: $TWITCH_CLIENT_ID" -X GET "https://api.twitch.tv/helix/users?login=$CHANNEL_NAME" | jq -r .data[0].id)
-# echo $CHANNEL_ID
-
-# to get the current tags:
-# TAGS=$(curl -s -H "Client-ID: $TWITCH_CLIENT_ID" -X GET "https://api.twitch.tv/helix/streams/tags?broadcaster_id=$CHANNEL_ID" | jq -c '[.data[].tag_id]')
-# echo $TAGS
-
-RESP=$(curl -s http://localhost:8080/auth/twitch)
-
-echo $RESP | jq
-
-CHANNEL_ID=$(echo $RESP | jq -r .channel_id)
-USER_ACCESS_TOKEN=$(echo $RESP | jq -r .user_access_token)
-
 # 6ea6bca4-4712-4ab9-a906-e3336a9d8039 english (not required)
 # 1400ca9c-84ea-414e-a85b-076a70d38ecf automotive
 # 77223888-8535-4614-974b-b1b2673456eb visual asmr
@@ -25,7 +7,15 @@ USER_ACCESS_TOKEN=$(echo $RESP | jq -r .user_access_token)
 # a6ff589a-33e5-4caf-8286-29dea98fc2e2 travel
 # 89e105c9-2c45-42a9-a5f0-fc1ea6e7ba8b outdoors
 
-curl -s -H "Client-ID: $TWITCH_CLIENT_ID" \
+RESP=$(curl -s http://localhost:8080/auth/twitch)
+echo $RESP | jq
+
+# extract the fields from the response
+CHANNEL_ID=$(echo $RESP | jq -r .channel_id)
+CLIENT_ID=$(echo $RESP | jq -r .client_id)
+USER_ACCESS_TOKEN=$(echo $RESP | jq -r .user_access_token)
+
+curl -s -H "Client-ID: $CLIENT_ID" \
   -H 'Content-Type: application/json' \
   -H "Authorization: Bearer $USER_ACCESS_TOKEN" \
   -X PUT "https://api.twitch.tv/helix/streams/tags?broadcaster_id=$CHANNEL_ID" \
--- bin/subscribe-to-webhooks.sh
+++ bin/subscribe-to-webhooks.sh
@@ -1,18 +1,13 @@
 #!/usr/bin/env bash
 
-source .env
-# CHANNEL_NAME=adanalife_
+RESP=$(curl -s http://localhost:8080/auth/twitch)
+echo $RESP
 
-# curl -s -H "Client-ID: $TWITCH_CLIENT_ID" \
-#   -H 'Content-Type: application/json' \
-#   -H "Authorization: Bearer $USER_ACCESS_TOKEN" \
-#   -X PUT "https://api.twitch.tv/helix/streams/tags?broadcaster_id=$CHANNEL_ID" \
-#   -d '{"tag_ids": ["1400ca9c-84ea-414e-a85b-076a70d38ecf","77223888-8535-4614-974b-b1b2673456eb","a4fac2cc-7cd4-44a6-b620-178182389a5b","a6ff589a-33e5-4caf-8286-29dea98fc2e2" ,"89e105c9-2c45-42a9-a5f0-fc1ea6e7ba8b"]}'
-
-CHANNEL_ID=$(curl -s http://localhost:8080/auth/twitch | jq -r .channel_id)
+CHANNEL_ID=$(echo $RESP | jq -r .channel_id)
+CLIENT_ID=$(echo $RESP | jq -r .client_id)
 
 curl -s \
-  -H "Client-ID: $TWITCH_CLIENT_ID" \
+  -H "Client-ID: $CLIENT_ID" \
   -H 'Content-Type: application/json' \
   -X POST https://api.twitch.tv/helix/webhooks/hub \
   -d "{\"hub.callback\": \"http://tripbot.dana.lol/webhooks\", \"hub.mode\": \"subscribe\", \"hub.topic\": \"https://api.twitch.tv/helix/users/follows?first=1&to_id=$CHANNEL_ID\"}"
