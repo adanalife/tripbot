f8ea83cf906ab2fed360970c3a65de1989c574bf
Dana Merrick <dana.merrick@postmates.com>
2019-11-29 16:05:00 -0600

    We don't need the shell script anymore

--- pkg/twitch/webhooks.go
+++ pkg/twitch/webhooks.go
@@ -2,26 +2,25 @@ package twitch
 
 import (
 	"log"
-	"os/exec"
-	"path"
 
 	"github.com/davecgh/go-spew/spew"
 	"github.com/dmerrick/danalol-stream/pkg/config"
 	terrors "github.com/dmerrick/danalol-stream/pkg/errors"
-	"github.com/dmerrick/danalol-stream/pkg/helpers"
 	"github.com/nicklaw5/helix"
 )
 
-// UpdateWebhookSubscriptions will call a shell script (lol) to
-// create new webhook subscriptions
+// UpdateWebhookSubscriptions will create new webhook subscriptions
 //TODO: use the twitch API instead of a shell script when possible
 func UpdateWebhookSubscriptions() {
-	// run the shell script to update webhook subscriptions
-	scriptPath := path.Join(helpers.ProjectRoot(), "bin/subscribe-to-webhooks.sh")
-	_, err := exec.Command(scriptPath, config.ExternalURL).Output()
+	//TODO: eventually use Secret param too
+	_, err := currentTwitchClient.PostWebhookSubscription(&helix.WebhookSubscriptionPayload{
+		Mode:         "subscribe",
+		Topic:        "https://api.twitch.tv/helix/users/follows?first=1&to_id=" + ChannelID,
+		Callback:     config.ExternalURL + "/webhooks/twitch/users/follows",
+		LeaseSeconds: 24 * 60 * 60, // 24h is the max allowed
+	})
 	if err != nil {
-		terrors.Log(err, "failed to run script")
-		return
+		terrors.Log(err, "failed to create webhook subscription")
 	}
 
 	// talk to twitch and see what the current webhooks are
