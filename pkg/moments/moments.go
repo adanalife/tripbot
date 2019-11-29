9faf0fe2a635fe510d65adc1bb129c63d940acd8
Dana Merrick <dana.merrick@postmates.com>
2019-11-28 20:59:37 -0600

    Adding new fields

--- pkg/moments/moments.go
+++ pkg/moments/moments.go
@@ -12,6 +12,13 @@ type Moment struct {
 	Lat         float64       `db:"lat"`
 	Lng         float64       `db:"lng"`
 	Rating      float64       `db:"rating"`
+	Address     string        `db:"address"`
+	Locality    string        `db:"locality"`
+	Region      string        `db:"region"`
+	Postcode    string        `db:"postcode"`
+	Country     string        `db:"country"`
+	Flagged     bool          `db:"flagged"`
+	TimeOffset  string        `db:"time_offset"`
 	NextMoment  sql.NullInt64 `db:"next_moment"`
 	PrevMoment  sql.NullInt64 `db:"prev_moment"`
 	DateCreated time.Time     `db:"date_created"`
