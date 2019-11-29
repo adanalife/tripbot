bb43fd20d72fe1e154199f213a19ba275b4903b2
Dana Merrick <dana.merrick@postmates.com>
2019-11-28 20:51:55 -0600

    Updates

--- db/migrate/006_create_moments.up.sql
+++ db/migrate/006_create_moments.up.sql
@@ -5,6 +5,12 @@ CREATE TABLE moments (
   prev_moment    INTEGER,
   lat            FLOAT DEFAULT 0,
   lng            FLOAT DEFAULT 0,
-  rating         FLOAT DEFAULT 0,
+  address        VARCHAR,
+  locality       VARCHAR,
+  region         VARCHAR,
+  postcode       VARCHAR,
+  country        VARCHAR,
+  flagged        BOOLEAN DEFAULT FALSE,
+  time_offset    VARCHAR(10),
   date_created   TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
