7941c139ba7166dd2d88aca83c5482a6ffc766ea
Dana Merrick <dana.merrick@postmates.com>
2019-12-08 19:35:49 -0600

    Progress

--- pkg/chatbot/handlers.go
+++ pkg/chatbot/handlers.go
@@ -1,27 +1,45 @@
 package chatbot
 
-// handles all chat messages
-func PrivateMessage(message twitch.PrivateMessage) {
-	username := message.User.Name
-
-	// log in the user
-	user := users.LoginIfNecessary(username)
-
-	if strings.HasPrefix(strings.ToLower(message.Message), "!help") {
+func runCommand(user users.User, message string) {
+	split := strings.Split(message, " ")
+	command := split[0]
+	params := split[1:]
+	switch command {
+	case "!help":
 		helpCmd(user)
-	}
-
-	if strings.HasPrefix(strings.ToLower(message.Message), "!uptime") {
+	case "!uptime":
 		uptimeCmd(user)
-	}
-
-	if strings.HasPrefix(strings.ToLower(message.Message), "!oldmiles") {
+	case "!uptime":
+		uptimeCmd(user)
+	case "!oldmiles":
 		if user.HasCommandAvailable() {
 			oldMilesCmd(user)
 		} else {
 			client.Say(config.ChannelName, followerMsg)
 		}
+	default:
+		err = fmt.Errorf("command %s not found", command)
 	}
+	if err != nil {
+		terrors.Log(err, "error running command")
+	}
+}
+
+// handles all chat messages
+func PrivateMessage(message twitch.PrivateMessage) {
+	username := message.User.Name
+	//TODO: we lose capitalization here, is that okay?
+	message := strings.ToLower(message.Message)
+
+	// check to see if the message is a command
+	//TODO: also include ones prefixed with whitespace?
+	if strings.HasPrefix(message) {
+		// log in the user
+		user := users.LoginIfNecessary(username)
+
+		runCommand(user, message)
+	}
+
 
 	// any of these should trigger the miles command
 	milesStrings := []string{
