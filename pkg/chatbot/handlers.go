873370c5d3e27a10d7d3c969ba0b6425911f534c
Dana Merrick <dana.merrick@postmates.com>
2019-12-08 19:37:47 -0600

    Cleaning up

--- pkg/chatbot/handlers.go
+++ pkg/chatbot/handlers.go
@@ -33,38 +33,10 @@ func PrivateMessage(message twitch.PrivateMessage) {
 
 	// check to see if the message is a command
 	//TODO: also include ones prefixed with whitespace?
-	if strings.HasPrefix(message) {
+	if strings.HasPrefix(message, "!") {
 		// log in the user
 		user := users.LoginIfNecessary(username)
 
 		runCommand(user, message)
 	}
-
-
-	// any of these should trigger the miles command
-	milesStrings := []string{
-		"!miles",
-		"!newmiles",
-	}
-	for _, s := range milesStrings {
-		if strings.HasPrefix(strings.ToLower(message.Message), s) {
-			if user.HasCommandAvailable() {
-				milesCmd(user)
-			} else {
-				client.Say(config.ChannelName, followerMsg)
-			}
-		}
-	}
-
-	// any of these should trigger the kilometres command
-	kilometresStrings := []string{
-		"!km",
-		"!kilometres",
-		"!kilometers",
-	}
-	for _, s := range kilometresStrings {
-		if strings.HasPrefix(strings.ToLower(message.Message), s) {
-			if user.HasCommandAvailable() {
-				kilometresCmd(user)
-			} else {
-				client.Say(config.ChannelName, followerMsg)
+}
