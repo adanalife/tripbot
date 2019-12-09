07bd5d9c5f3bf98eac059d9b40ff390ed8b73c0b
Dana Merrick <dana.merrick@postmates.com>
2019-12-09 14:43:28 -0800

    Adding command parsing to whispers

--- pkg/chatbot/handlers.go
+++ pkg/chatbot/handlers.go
@@ -1,19 +1,15 @@
 package chatbot
 
 import (
-	"fmt"
 	"log"
 	"strings"
 
 	"github.com/dmerrick/danalol-stream/pkg/config"
-	terrors "github.com/dmerrick/danalol-stream/pkg/errors"
 	"github.com/dmerrick/danalol-stream/pkg/users"
 	"github.com/gempir/go-twitch-irc/v2"
 )
 
-func runCommand(user users.User, message string) {
-	var err error
-
+func runCommand(user users.User, message string, whisper bool) {
 	split := strings.Split(message, " ")
 	command := split[0]
 	params := split[1:]
@@ -21,7 +17,7 @@ func runCommand(user users.User, message string) {
 	switch command {
 	case "!help":
 		// whisper if the user ran !help with no params
-		whisper := (len(params) == 0)
+		whisper = (whisper || len(params) == 0)
 		helpCmd(&user, params, whisper)
 	case "!uptime":
 		uptimeCmd(&user)
@@ -117,30 +113,21 @@ func runCommand(user users.User, message string) {
 		} else {
 			client.Say(config.ChannelName, followerMsg)
 		}
-	default:
-		err = fmt.Errorf("command %s not found", command)
-	}
-	if err != nil {
-		terrors.Log(err, "error running command")
 	}
 }
 
 // handles all chat messages
 func PrivateMessage(msg twitch.PrivateMessage) {
 	username := msg.User.Name
+	//TODO: also include ones prefixed with whitespace?
 	//TODO: we lose capitalization here, is that okay?
 	message := strings.ToLower(msg.Message)
 
-	// check to see if the message is a command
-	//TODO: also include ones prefixed with whitespace?
-	//TODO: not all commands start with "!"s
-	if strings.HasPrefix(message, "!") {
-		// log in the user
-		user := users.LoginIfNecessary(username)
+	// log in the user
+	user := users.LoginIfNecessary(username)
 
-		//TODO is it okay that this isn't a pointer?
-		runCommand(*user, message)
-	}
+	// run the command if possible
+	runCommand(*user, message, false)
 }
 
 // this event fires when a user joins the channel
@@ -160,9 +147,28 @@ func UserPart(partMessage twitch.UserPartMessage) {
 // }
 
 // if the message comes from me, then post the message to chat
-func Whisper(message twitch.WhisperMessage) {
-	log.Println("whisper from", message.User.Name, ":", message.Message)
-	if message.User.Name == strings.ToLower(config.ChannelName) {
-		client.Say(config.ChannelName, message.Message)
+func Whisper(msg twitch.WhisperMessage) {
+	log.Println("whisper from", msg.User.Name, ":", msg.Message)
+
+	// allow the channel owner to speak through the bot's mouth
+	if msg.User.Name == strings.ToLower(config.ChannelName) {
+		// not using the "message" var here cause I want to preserve
+		// capitalization in the parroted message
+		client.Say(config.ChannelName, msg.Message)
+		return
+	}
+
+	username := msg.User.Name
+	message := strings.ToLower(msg.Message)
+
+	// log in the user
+	user := users.LoginIfNecessary(username)
+
+	if strings.HasPrefix(message, "!help") {
+		runCommand(*user, message, true) // send as a whisper
+		return
+	} else {
+		msg := "I'm sorry, I haven't yet been taught how to respond to that. Try \"!help commands\"."
+		client.Whisper(username, msg)
 	}
 }
