a088b3a1aa9c53e279c369a87fa3f8725c39f105
Dana Merrick <dana.merrick@postmates.com>
2020-01-25 04:50:02 +0000

    Updating start script to work on linux

--- bin/start-obs.sh
+++ bin/start-obs.sh
@@ -2,9 +2,14 @@
 
 PID_FILE=OBS/OBS.pid
 
+# this is just for the hacky NVENC setup
+export LD_LIBRARY_PATH="/home/dmerrick/other_projects/ffmpeg-nvenc/ffmpeg-nvenc/lib":$LD_LIBRARY_PATH
+
 echo "starting OBS..."
 #TODO: should be --startstreaming
-nice -n "-15" /Applications/OBS.app/Contents/MacOS/OBS -start >> log/obs-$(date "+%Y-%m-%d").log 2>&1 &
+/home/dmerrick/other_projects/ffmpeg-nvenc/ffmpeg-nvenc/bin/obs "$@"
+#nice -n "-15" /Applications/OBS.app/Contents/MacOS/OBS -start "$@" >> log/obs-$(date "+%Y-%m-%d").log 2>&1 &
+nice -n "-15" ./obs -start "$@" >> log/obs-$(date "+%Y-%m-%d").log 2>&1 &
 
 # save pid to file
 PID=$!
