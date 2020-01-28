0fbbfa4e9e471fff6842d4dc6831c82d79c45577
Dana Merrick <dana.merrick@postmates.com>
2020-01-28 19:02:06 +0000

    Further improvements

--- bin/current-file.sh
+++ bin/current-file.sh
@@ -8,13 +8,19 @@
 # echo "2018_0522_201919_010_opt.MP4"
 # ssh void -- "cd other_projects/danalol-stream && /Users/dmerrick/other_projects/danalol-stream/bin/current-file.sh"
 
-#TODO: check for presense of file here
+OBS_PIDFILE="OBS/OBS.pid"
 
-output=$(lsof -p $(cat OBS/OBS.pid) 2>/dev/null)
+#TODO: check for presense of file here
+if [ ! -f $OBS_PIDFILE ]; then
+  echo "OBS pidfile not found. Is OBS running?"
+  exit 2
+fi
+
+output=$(lsof -p $(cat $OBS_PIDFILE) 2>/dev/null)
 if [ $? -eq 0 ]; then
   #TODO: error message if more than 1 result from search
   echo $output | grep -i '\.MP4' | sed -e 's/^.*2018_/2018_/'
 else
-  echo "No MP4s were found in lsof output"
+  echo "No MP4s were found in lsof output. Check that the PID is correct and that OBS is playing videos"
   exit 3
 fi
