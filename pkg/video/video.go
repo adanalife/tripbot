b0f6fbd7e0e74ce91e3b263867a29ade160e3237
Dana Merrick <dana.merrick@postmates.com>
2020-01-29 05:38:49 +0000

    Improving output

--- pkg/video/video.go
+++ pkg/video/video.go
@@ -63,12 +63,13 @@ func figureOutCurrentVideo() string {
 	// run the shell script to get currently-playing video
 	scriptPath := path.Join(helpers.ProjectRoot(), "bin/current-file.sh")
 	out, err := exec.Command(scriptPath).Output()
+	outString := strings.TrimSpace(string(out))
 	if err != nil {
 		terrors.Log(err, "failed to get currently-playing video")
-		log.Println(string(out))
+		log.Println(outString)
 		return ""
 	}
-	return strings.TrimSpace(string(out))
+	return outString
 }
 
 func showNoGPSImage() {
