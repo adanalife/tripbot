8f084b8d68fd4984cd7f95442252ba4070ff14c6
Dana Merrick <dana.merrick@postmates.com>
2020-01-30 21:35:42 +0000

    Progress

--- pkg/background/left-rotator.go
+++ pkg/background/left-rotator.go
@@ -1,12 +1,15 @@
 package background
 
 import (
+	"fmt"
 	"math/rand"
 	"path"
 	"time"
 
+	"github.com/davecgh/go-spew/spew"
 	"github.com/dmerrick/danalol-stream/pkg/helpers"
 	"github.com/dmerrick/danalol-stream/pkg/onscreens"
+	"github.com/dmerrick/danalol-stream/pkg/users"
 )
 
 // super long duration cause this is always on
@@ -15,6 +18,21 @@ var leftRotatorFile = path.Join(helpers.ProjectRoot(), "OBS/leaderboard.txt")
 
 var LeftRotator *onscreens.Onscreen
 
+var possibleMessages = []string{
+	"Looking for artist for emotes and more",
+	"Want to help the stream? Fill out the !survey",
+	"Want to help the stream? Fill out the !survey",
+	"Want to help the stream? Fill out the !survey",
+	"Twitch Prime subs keep us on air :D",
+	"Earn miles for every minute you watch (!miles)",
+	"I won't be offended if you play your own music",
+	"Music by Soma.fm (!song)",
+	"Use !report to report stream issues",
+	"Try and !guess what state we're in",
+	"Where are we? (!location)",
+	"LEADER",
+}
+
 func InitLeftRotator() {
 	LeftRotator = onscreens.New(leftRotatorFile)
 	go leftRotatorLoop()
@@ -22,9 +40,7 @@ func InitLeftRotator() {
 
 func leftRotatorLoop() {
 	for { // forever
-		if rand.Intn(10) == 0 {
-			LeftRotator.Show(leftRotatorContent(), leftRotatorDuration)
-		}
+		LeftRotator.Show(leftRotatorContent(), leftRotatorDuration)
 		time.Sleep(time.Duration(30 * time.Second))
 	}
 }
@@ -33,58 +49,23 @@ func leftRotatorLoop() {
 func leftRotatorContent() string {
 	var output string
 
-	output = "test message"
+	// show a special, very rare message
+	if rand.Intn(10000) == 0 {
+		return "You found the rare message! Make a clip for a prize!"
+	}
+
+	// pick a random message
+	message := possibleMessages[rand.Intn(len(possibleMessages))]
+
+	switch message {
+	case "LEADER":
+		spew.Dump(users.Leaderboard[:1])
+		// get the first leader in the leaderboard
+		leader := users.Leaderboard[:1][0]
+		output = fmt.Sprintf("%s is leader with %s miles (!leaderboard)", leader[1], leader[0])
+	default:
+		output = message
+	}
 
 	return output
 }
-
-// sub_goal = "10/10"
-// donation_goal = "15/150"
-
-// leaderboard_file = "./OBS/leaderboard-copy.txt"
-// message_file = "./OBS/left-message.txt"
-
-// possible_messages = [
-//   "Looking for artist for emotes and more",
-//   "Want to help the stream? Fill out the !survey",
-//   "Want to help the stream? Fill out the !survey",
-//   "Want to help the stream? Fill out the !survey",
-//   "Sub goal #2 (map overlay): coming soon!",
-//   "Donation goal (!temperature): #{donation_goal}",
-//   "Twitch Prime subs keep us on air :D",
-//   "Earn miles for every minute you watch (!miles)",
-//   "I won't be offended if you play your own music",
-//   "Music by Soma.fm (!song)",
-//   "Use !report to report stream issues",
-//   "Try and !guess what state we're in",
-//   # "Tripbot loves you <3 (!tripbot)",
-//   "Where are we? (!location)",
-//   "LEADER",
-//   "RARE",
-// ]
-
-// puts "starting #$PROGRAM_NAME script"
-
-// loop do
-//   # pick a random message
-//   current = possible_messages.sample
-
-//   # just for fun: a very rare message
-//   if current == "RARE"
-//     next unless rand(1000).zero?
-//     current = "You found the rare message! Make a clip for a prize!"
-//   end
-
-//   if current == "LEADER"
-//     # fetch the current leader
-//     begin
-//       miles, leader = File.read(File.expand_path(leaderboard_file)).split("\n")[1].split(":")
-//     rescue
-//       next
-//     end
-//     next unless leader
-//     current = "#{leader.strip} is leader with #{miles} (!leaderboard)"
-//   end
-
-//   File.write(File.expand_path(message_file), current)
-//   sleep 45
