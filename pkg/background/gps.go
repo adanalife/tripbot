36fe745b1f52a64183deac0e341a581bd6b20a98
Dana Merrick <dana.merrick@postmates.com>
2020-01-30 23:50:31 +0000

    Adding image support

---
+++ pkg/background/gps.go
@@ -0,0 +1,20 @@
+package background
+
+import (
+	"log"
+	"path"
+	"time"
+
+	"github.com/dmerrick/danalol-stream/pkg/helpers"
+	"github.com/dmerrick/danalol-stream/pkg/onscreens"
+)
+
+var GPSImage *onscreens.Onscreen
+
+var gpsDuration = time.Duration(150 * time.Second)
+var gpsImageFile = path.Join(helpers.ProjectRoot(), "OBS/right-message.txt")
+
+func InitGPSImage() {
+	log.Println("Creating GPS image onscreen")
+	GPSImage = onscreens.NewImage(gpsImageFile)
+}
