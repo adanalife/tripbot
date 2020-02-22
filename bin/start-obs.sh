4494e4f9d02b59cf92ea3b0013c517fb490df655
Dana Merrick <dana.merrick@postmates.com>
2020-02-22 21:05:15 +0000

    Okay let's use snap

--- bin/start-obs.sh
+++ bin/start-obs.sh
@@ -9,11 +9,12 @@ if [ $(uname) == 'Darwin' ]; then
   nice -n "-15" /Applications/OBS.app/Contents/MacOS/OBS -start "$@" >> log/obs-$(date "+%Y-%m-%d").log 2>&1 &
 else
   # this is just for the hacky NVENC setup
-  export LD_LIBRARY_PATH="/home/dmerrick/other_projects/ffmpeg-nvenc/ffmpeg-nvenc/lib":$LD_LIBRARY_PATH
+  # export LD_LIBRARY_PATH="/home/dmerrick/other_projects/ffmpeg-nvenc/ffmpeg-nvenc/lib":$LD_LIBRARY_PATH
+  # /home/dmerrick/other_projects/ffmpeg-nvenc/ffmpeg-nvenc/bin/obs -verbose -start "$@" >> log/obs-$(date "+%Y-%m-%d").log 2>&1 &
   # set DISPLAY if unset
   export DISPLAY="${DISPLAY:-:0}"
   #nice -n "-15" obs -start "$@" >> log/obs-$(date "+%Y-%m-%d").log 2>&1 &
-  /home/dmerrick/other_projects/ffmpeg-nvenc/ffmpeg-nvenc/bin/obs -start "$@" >> log/obs-$(date "+%Y-%m-%d").log 2>&1 &
+  /snap/bin/obs-studio -verbose -start "$@" >> log/obs-$(date "+%Y-%m-%d").log 2>&1 &
 fi
 
 # save pid to file
