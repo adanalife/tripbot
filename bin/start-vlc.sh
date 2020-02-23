0782f9de093efc6b9eb9eefdbb47c054780daa15
Dana Merrick <dana.merrick@postmates.com>
2020-02-23 16:42:54 +0000

    Adding start script for VLC

---
+++ bin/start-vlc.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+PID_FILE=OBS/VLC.pid
+
+echo "starting VLC..."
+
+if [ $(uname) == 'Darwin' ]; then
+  echo "TODO: build this out"
+  exit 2
+else
+  export DISPLAY="${DISPLAY:-:0}"
+  vlc >> log/vlc-$(date "+%Y-%m-%d").log 2>&1 &
+fi
+
+# save pid to file
+PID=$!
+echo $PID > $PID_FILE
+
+echo "PID is: ${PID}"
+
+# wait for the background job to end
+wait $PID
+rm $PID_FILE
