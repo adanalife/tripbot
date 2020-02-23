c921c5ef1cdfff19496717ec579b66262f1d6f54
Dana Merrick <dana.merrick@postmates.com>
2020-02-23 20:40:41 +0000

    Refactored into a helper method

--- pkg/background/chat.go
+++ pkg/background/chat.go
@@ -32,7 +32,6 @@ func AddChatLine(username, line string, t time.Time) {
 // chatContent creates the content for the chat
 func chatContent() string {
 	var output string
-	var endpoint int
 
 	size := 5
 	// check to see if we even have enough lines
@@ -42,30 +41,38 @@ func chatContent() string {
 	// get the last lines
 	lines := ChatLines[len(ChatLines)-size:]
 
-	// spew.Dump(ChatLines)
-	// spew.Dump(lines)
-
 	// add all the lines together
-	for _, fullLine := range lines {
-		line := ""
-		lineLength := len(fullLine)
-		if lineLength > lineBreak {
-			// include the first characters
-			line = fullLine[:lineBreak]
-			// add a newline and an indent
-			line += "\n  "
-			// we want to add one more line (subracting 2 for the indent)
-			endpoint = lineBreak + lineBreak - 2
-			// but sometimes the endpoint is beyond the size of the line
-			if endpoint > lineLength {
-				// in which case we should just use the end of the line
-				endpoint = lineLength
-			}
-			line += fullLine[lineBreak:endpoint]
-			//TODO: consider adding a "..." after really long messages
-		}
-		output = output + "\n" + line
+	for _, line := range lines {
+		output = output + "\n" + breakUpLine(line)
 	}
 
 	return output
+}
+
+// breakUpLine takes one long line and breaks it up
+// into two lines of fixed width
+func breakUpLine(fullLine string) string {
+	var endpoint int
+	line := ""
+	lineLength := len(fullLine)
+
+	// exit early if we don't need to break up the line
+	if lineLength <= lineBreak {
+		return fullLine
+	}
+
+	// include the first characters
+	line = fullLine[:lineBreak]
+	// add a newline and an indent
+	line += "\n  "
+	// we want to add one more line (subracting 2 for the indent)
+	endpoint = lineBreak + lineBreak - 2
+	// but sometimes the endpoint is beyond the size of the line
+	if endpoint > lineLength {
+		// in which case we should just use the end of the line
+		endpoint = lineLength
+	}
+	line += fullLine[lineBreak:endpoint]
+	//TODO: consider adding a "..." after really long messages
+	return line
 }
