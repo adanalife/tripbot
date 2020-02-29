fbd189e47901b65c6901776078653fa5f6e34319
Dana Merrick <dana.merrick@postmates.com>
2020-02-29 05:22:55 +0000

    Consistent file naming scheme

--- bin/start-vlc.sh
+++ bin/start-vlc.sh
@@ -10,7 +10,7 @@ if [ $(uname) == 'Darwin' ]; then
 else
   export DISPLAY="${DISPLAY:-:0}"
   VIDEO_DIR="/home/dmerrick/Videos/Dashcam/_all"
-  nice -n "-15" vlc --fullscreen "${VIDEO_DIR}" >> log/vlc-$(date "+%Y-%m-%d").log 2>&1 &
+  nice -n "-15" vlc --fullscreen "${VIDEO_DIR}" >> log/vlc-$(date "+%Y%m%d").log 2>&1 &
 fi
 
 # save pid to file
