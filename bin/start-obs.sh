c600c9e5ab534f7496486c858a4992e5dc8187cb
Dana Merrick <dana.merrick@postmates.com>
2020-03-14 23:49:03 +0000

    Adding OBS_START_STREAMING option

--- bin/start-obs.sh
+++ bin/start-obs.sh
@@ -3,7 +3,6 @@
 PID_FILE=OBS/OBS.pid
 
 echo "starting OBS..."
-#TODO: should be --startstreaming
 
 if [ $(uname) == 'Darwin' ]; then
   nice -n "-15" /Applications/OBS.app/Contents/MacOS/OBS -start "$@" >> log/obs-$(date "+%Y-%m-%d").log 2>&1 &
@@ -13,8 +12,11 @@ else
   # /home/dmerrick/other_projects/ffmpeg-nvenc/ffmpeg-nvenc/bin/obs -verbose -start "$@" >> log/obs-$(date "+%Y-%m-%d").log 2>&1 &
   # set DISPLAY if unset
   export DISPLAY="${DISPLAY:-:0}"
-  #nice -n "-15" obs -start "$@" >> log/obs-$(date "+%Y-%m-%d").log 2>&1 &
-  /snap/bin/obs-studio -verbose -start "$@" >> log/obs-$(date "+%Y-%m-%d").log 2>&1 &
+  if [ "$OBS_START_STREAMING" = "true" ] ; then
+	/snap/bin/obs-studio --verbose --startstreaming "$@" >> log/obs-$(date "+%Y-%m-%d").log 2>&1 &
+  else
+	nice -n "-15" obs -start "$@" >> log/obs-$(date "+%Y-%m-%d").log 2>&1 &
+  fi
 fi
 
 # save pid to file
