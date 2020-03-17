80067e2eeddb13ca541cb9028d322881a3f88739
Dana Merrick <dana.merrick@gmail.com>
2020-03-17 13:50:22 -0500

    Setting project name

--- bin/build-docker-image.sh
+++ bin/build-docker-image.sh
@@ -3,4 +3,9 @@
 #TODO: if cmd/tripbot/Dockerfile doesnt exist, exit early
 
 # docker build -t tripbot:latest . -f cmd/tripbot/Dockerfile
-docker-compose --project-directory . -f cmd/tripbot/docker-compose.yml --env-file .env.staging build
+docker-compose \
+  -p danalol-stream \
+  --project-directory . \
+  -f cmd/tripbot/docker-compose.yml \
+  --env-file .env.staging \
+  build
