5972c494deaff9e9a55831118a62e63915a57442
Dana Merrick <dana.merrick@gmail.com>
2020-03-18 00:25:10 -0500

    Adding a comment

--- infra/docker/init.sql
+++ infra/docker/init.sql
@@ -1,3 +1,5 @@
+-- this script is executed when the postgres image boots
+-- c.p. https://stackoverflow.com/a/51759325/1070080
 CREATE DATABASE "tripbot_docker";
 CREATE USER tripbot_docker WITH PASSWORD 'hunter2';
 GRANT ALL PRIVILEGES ON DATABASE "tripbot_docker" to tripbot_docker;
