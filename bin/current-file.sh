92d26b1cf6677adb051d1eed6522810cca7cee79
Dana Merrick <dana.merrick@gmail.com>
2020-03-19 13:27:52 -0500

    Removing commented code

--- bin/current-file.sh
+++ bin/current-file.sh
@@ -2,19 +2,12 @@
 
 # this script finds the currently-playing video file
 
-# SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
-# lsof -p $(cat ${SCRIPTPATH}/../run/OBS.pid) | grep -i '\.MP4' | sed -e 's/^.*2018_/2018_/'
-
-# echo "2018_0522_201919_010_opt.MP4"
-# ssh void -- "cd other_projects/danalol-stream && /Users/dmerrick/other_projects/danalol-stream/bin/current-file.sh"
-
 #TODO: better explain why we use VLC here
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
   PIDFILE="run/VLC.pid"
 elif [[ "$OSTYPE" == "darwin"* ]]; then
   PIDFILE="run/OBS.pid"
 fi
-
 
 #TODO: check for presence of file here
 if [ ! -f $PIDFILE ]; then
