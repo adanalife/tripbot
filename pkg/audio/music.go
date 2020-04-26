71df65523588ba25bb7fa66af6b32093e3863f30
Dana Merrick <dana.merrick@gmail.com>
2020-04-26 14:39:33 -0400

    Reducing Sentry spam

--- pkg/audio/music.go
+++ pkg/audio/music.go
@@ -40,7 +40,8 @@ func connect() {
 	// Connect to MPD server
 	mpdConn, err = mpd.Dial("tcp", config.MpdServerHost)
 	if err != nil {
-		terrors.Log(err, "Error connecting to MPD")
+		//TODO: an exponential backoff would be nice here
+		log.Println(aurora.Red("Error connecting to MPD"))
 	}
 }
 
