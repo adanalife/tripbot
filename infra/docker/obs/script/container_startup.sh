018413c55ff8a63763d28ff1c2d6558fa39e8dad
Dana Merrick <dana.merrick@gmail.com>
2020-06-03 19:02:44 -0400

    Progress!

--- infra/docker/obs/script/container_startup.sh
+++ infra/docker/obs/script/container_startup.sh
@@ -3,13 +3,11 @@ OUR_IP=$(hostname -i)
 
 # start VNC server (Uses VNC_PASSWD Docker ENV variable)
 mkdir -p $HOME/.vnc && echo "$VNC_PASSWD" | vncpasswd -f > $HOME/.vnc/passwd
-vncserver :0 -localhost no -nolisten -rfbauth $HOME/.vnc/passwd -xstartup /opt/x11vnc_entrypoint.sh
-# start noVNC web server
-# /opt/noVNC/utils/launch.sh --listen 5901 &
+vncserver :0 -localhost no -nolisten -rfbauth $HOME/.vnc/passwd -xstartup /opt/tripbot/infra/docker/obs/script/x11vnc_entrypoint.sh
 
 echo -e "\n\n------------------ VNC environment started ------------------"
 echo -e "\nVNCSERVER started on DISPLAY= $DISPLAY \n\t=> connect via VNC viewer with $OUR_IP:5900"
-# echo -e "\nnoVNC HTML client started:\n\t=> connect via http://$OUR_IP:5901/?password=$VNC_PASSWD\n"
+echo -e "\nvlc-server started:\n\t=> connect via http://$OUR_IP:8088\n" #TODO: make this port a var
 
 if [ -z "$1" ]; then
   tail -f /dev/null
