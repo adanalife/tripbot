c8a19c4baafa8c2e1a8aebd247be90cf0d0e1dca
Dana Merrick <dana.merrick@gmail.com>
2020-06-04 09:56:35 -0400

    Improving obs container log output

--- script/container_startup.sh
+++ script/container_startup.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 
-# this script is the container entrypoint
-# for the OBS container
+# this script is the container entrypoint for the OBS container
 
 OUR_IP=$(hostname -i)
-
 
 # start VNC server (Uses VNC_PASSWD Docker ENV variable)
 mkdir -p "$HOME/.vnc" && echo "$VNC_PASSWD" | vncpasswd -f > "$HOME/.vnc/passwd"
 vncserver :0 -localhost no -nolisten -rfbauth "$HOME/.vnc/passwd" -xstartup /opt/tripbot/script/x11vnc_entrypoint.sh
 
-echo -e "\n\n------------------ VNC environment started ------------------"
-echo -e "\nVNCSERVER started on DISPLAY= $DISPLAY \n\t=> connect via VNC viewer with $OUR_IP:5900"
-echo -e "\nvlc-server started:\n\t=> connect via http://$OUR_IP:8088\n" #TODO: make this port a var
+echo -e "\n\n------------------ OBS environment started ------------------"
+echo -e "\nVNC server started: \n\t=> connect via VNC at vnc://$OUR_IP:5900"
+echo -e "\nvlc-server started:\n\t=> connect via http://$OUR_IP:8088/vlc/current\n"
+echo -e "\nOBS started:\n\t=> view at https://twitch.tv/$CHANNEL_NAME\n"
 
-#TODO: use env vars to print a link to the twitch channel
-#TODO: tail a useful log in here
+if [ ! -z "${STREAM_KEY}" ]; then
+  echo -e "\n\nStream is LIVE!"
+fi
 
 if [ -z "$1" ]; then
-  tail -f /dev/null
+  echo -e "\n\n--------------------- VLC server log ------------------------"
+  tail -F /opt/tripbot/log/vlc*.log
 else
   # unknown option ==> call command
   echo -e "\n\n------------------ EXECUTE COMMAND ------------------"
