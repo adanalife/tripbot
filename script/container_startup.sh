84bb9c443ac3cff39c88d7c8ba919ca070d7fcac
Dana Merrick <dana.merrick@gmail.com>
2020-06-04 15:45:49 -0400

    Improving container startup

--- script/container_startup.sh
+++ script/container_startup.sh
@@ -9,12 +9,12 @@ mkdir -p "$HOME/.vnc" && echo "$VNC_PASSWD" | vncpasswd -f > "$HOME/.vnc/passwd"
 vncserver :0 -localhost no -nolisten -rfbauth "$HOME/.vnc/passwd" -xstartup /opt/tripbot/script/x11vnc_entrypoint.sh
 
 echo -e "\n\n------------------ OBS environment started ------------------"
-echo -e "\nVNC server started: \n\t=> connect via VNC at vnc://$OUR_IP:5900"
+echo -e "\nVNC server started:\n\t=> connect via VNC at vnc://$OUR_IP:5900"
 echo -e "\nvlc-server started:\n\t=> connect via http://$OUR_IP:8088/vlc/current\n"
 echo -e "\nOBS started:\n\t=> view at https://twitch.tv/$CHANNEL_NAME\n"
 
 if [ ! -z "${STREAM_KEY}" ]; then
-  echo -e "\n\nStream is LIVE!"
+  echo -e "\n\nStream is \e[31mLIVE\e[0m!" # red text
 fi
 
 if [ -z "$1" ]; then
