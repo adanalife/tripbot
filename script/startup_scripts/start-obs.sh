88c22d3ac2937c7bcf4513c19444799bc884b7d7
Dana Merrick <dana.merrick@gmail.com>
2020-06-03 21:07:34 -0400

    Moving things around

--- script/startup_scripts/start-obs.sh
+++ script/startup_scripts/start-obs.sh
@@ -1,18 +1,23 @@
 #!/usr/bin/env bash
 
-# set up stream key and scenes
-# RUN mkdir -p /root/.config/obs-studio/basic/profiles/Untitled/ /root/.config/obs-studio/basic/scenes/
 # COPY config/basic.ini config/service.json /root/.config/obs-studio/basic/profiles/Untitled/
 # COPY config/global.ini /root/.config/obs-studio/global.ini
 # COPY config/Dashcam_Scenes.docker.json /root/.config/obs-studio/basic/scenes/Untitled.json
-#TODO: instead of naming these both Untitled, edit ~/.config/obs-studio/global.ini
-
-# copy configs over
-cp -r /opt/tripbot/config/obs-studio /root/.config/
 
 export FIXTHIS="pass this in via docker"
 
+cd /opt/tripbot/configs/obs-studio
+
+# copy configs over
+#TODO: instead of naming these both Untitled, edit ~/.config/obs-studio/global.ini
+cp basic.ini /root/.config/obs-studio/basic/profiles/Untitled
+cp Dashcam_Scenes.linux.json /root/.config/obs-studio/basic/scenes/Untitled.json
+cp global.ini /root/.config/obs-studio/global.ini
+
 # set the streamkey from ENV var
-sed -i "s/STREAMKEY/$FIXTHIS/" /root/.config/obs-studio/basic/profiles/Untitled/service.json
+sed -e "s/STREAMKEY/$FIXTHIS/" service.json /root/.config/obs-studio/basic/profiles/Untitled/service.json
+
+#TODO: make it so we don't have to rename it here
+cp /root/.config/obs-studio/Dashcam_Scenes.docker.json /root/.config/obs-studio/basic/scenes/Untitled.json
 
 obs --startstreaming
