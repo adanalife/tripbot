5c5b11189b49330dccffb58ab8b055aa71c24fb5
Dana Merrick <dana.merrick@gmail.com>
2020-06-03 20:14:51 -0400

    Progress

--- infra/docker/obs/script/startup_scripts/start-vlc.sh
+++ infra/docker/obs/script/startup_scripts/start-vlc.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
 
+# hack VLC so we can run it as root
+# c.p. https://unix.stackexchange.com/a/199422/202812
+sed -i 's/geteuid/getppid/' /usr/bin/vlc
+
+# hack to make fontconfig happy
+#TODO: fix this
+export FONTCONFIG_PATH=/etc/fonts
+
+# compile vlc-server
 cd /opt/tripbot
-go build -o bin/vlc-server cmd/vlc-server/vlc-server.go
-bin/vlc-server
+go build -o bin/vlc-server cmd/vlc-server/vlc-server.go | tee -a log/build-vlc.log 2>&1
+
+# run vlc-server
+bin/vlc-server | tee -a log/start-vlc.log 2>&1
