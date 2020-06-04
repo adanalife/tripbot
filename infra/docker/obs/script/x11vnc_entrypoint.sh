ef02300c8d2219db408b4c568267201b3dd0bcd8
Dana Merrick <dana.merrick@gmail.com>
2020-06-03 20:39:48 -0400

    Improvements

--- infra/docker/obs/script/x11vnc_entrypoint.sh
+++ infra/docker/obs/script/x11vnc_entrypoint.sh
@@ -2,8 +2,9 @@
 # fix for clipboard being passed through
 vncconfig -nowin &
 
-if ls /opt/startup_scripts/*.sh 1> /dev/null 2>&1; then
-  for f in /opt/startup_scripts/*.sh; do
-    bash "$f" -H || (echo "Error with $f: $?" >> /var/log/x11vnc_entrypoint.log)
+if ls /opt/tripbot/script/startup_scripts/*.sh 1> /dev/null 2>&1; then
+  for f in /opt/tripbot/script/startup_scripts/*.sh; do
+    echo "running $f" >> /var/log/x11vnc_entrypoint.log
+    bash "$f" || (echo "Error with $f: $?" >> /var/log/x11vnc_entrypoint.log) &
   done
 fi
