94cbd3e2788dc2557172cbb92430077013ab2744
Dana Merrick <dana.merrick@gmail.com>
2020-06-03 21:23:37 -0400

    Updates

--- script/startup_scripts/start-obs.sh
+++ script/startup_scripts/start-obs.sh
@@ -1,18 +1,19 @@
 #!/usr/bin/env bash
 
-export FIXTHIS="pass this in via docker"
+#TODO: fail if STREAM_KEY isn't present
 
 cd /opt/tripbot/configs/obs-studio
 
+mkdir -p /root/.config/obs-studio/basic/profiles/Untitled/ /root/.config/obs-studio/basic/scenes/
+
 # copy configs over
 #TODO: instead of naming these both Untitled, edit ~/.config/obs-studio/global.ini
-mkdir -p /root/.config/obs-studio/basic/profiles/Untitled/ /root/.config/obs-studio/basic/scenes/
 cp global.ini /root/.config/obs-studio/global.ini
 cp basic.ini /root/.config/obs-studio/basic/profiles/Untitled
 cp service.json /root/.config/obs-studio/basic/profiles/Untitled
 cp Dashcam_Scenes.docker.json /root/.config/obs-studio/basic/scenes/Untitled.json
 
 # set the streamkey from ENV var
-sed -i "s/STREAMKEY/$FIXTHIS/" /root/.config/obs-studio/basic/profiles/Untitled/service.json
+sed -i "s/STREAMKEY/$STREAM_KEY/" /root/.config/obs-studio/basic/profiles/Untitled/service.json
 
 obs --startstreaming
