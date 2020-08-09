b2aa243e79fd346d107d6bd4d6dba4213fc5df91
Dana Merrick <dana.merrick@gmail.com>
2020-08-09 17:45:16 -0400

    Progress

---
+++ pkg/onscreens-client/onscreens-client.go
@@ -0,0 +1,42 @@
+package onscreensClient
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/adanalife/tripbot/pkg/config"
+	terrors "github.com/adanalife/tripbot/pkg/errors"
+)
+
+//TODO: eventually support HTTPS
+var onscreensServerURL = "http://" + config.VlcServerHost
+
+// CurrentlyPlaying finds the currently-playing video path
+func SetMiddleText(msg string) string {
+	url := onscreensServerURL + "/onscreens/middle"
+	url = fmt.Sprintf("%s?msg=\"%s\"", url, msg)
+	_, err := getUrl(url)
+	if err != nil {
+		terrors.Log(err, "error setting middle onscreen")
+		return err
+	}
+	return nil
+}
+
+//TODO: move this to a common location
+func getUrl(url string) (string, error) {
+	response, err := http.Get(url)
+	if err != nil {
+		terrors.Log(err, "error connecting to VLC server")
+		return "", err
+	} else {
+		defer response.Body.Close()
+		contents, err := ioutil.ReadAll(response.Body)
+		if err != nil {
+			terrors.Log(err, "error reading response from VLC server")
+			return "", err
+		}
+		return string(contents), nil
+	}
+}
