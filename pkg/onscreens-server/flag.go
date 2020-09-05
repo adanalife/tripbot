1046be2f0e6cfa49579a97fb70e9502cdf62fcc7
Dana Merrick <dana.merrick@gmail.com>
2020-09-05 12:50:18 -0400

    Progress

--- pkg/onscreens-server/flag.go
+++ pkg/onscreens-server/flag.go
@@ -1,10 +1,16 @@
 package onscreensServer
 
 import (
+	"fmt"
 	"log"
+	"os"
 	"path"
+	"strings"
+	"time"
 
 	"github.com/adanalife/tripbot/pkg/config"
+	terrors "github.com/adanalife/tripbot/pkg/errors"
+	"github.com/adanalife/tripbot/pkg/helpers"
 )
 
 var FlagImage *Onscreen
@@ -15,4 +21,62 @@ var FlagImageFile = path.Join(config.RunDir, "flag.png")
 func InitFlagImage() {
 	log.Println("Creating flag image onscreen")
 	FlagImage = NewImage(FlagImageFile)
+}
+
+func ShowFlag(dur time.Duration) {
+	//TODO: this should trigger when a state change event fires instead of every time we run this
+	updateFlagFile()
+	// actually display the flag
+	//TODO: this needs to be a vlcClient thing
+	onscreensClient.ShowFlag(10 * time.Second)
+}
+
+// updateFlagFile replaces the current flag image with the current state flag
+func updateFlagFile() {
+	if helpers.FileExists(FlagImageFile) {
+		if config.Verbose {
+			log.Printf("removing %s because it already exists", FlagImageFile)
+		}
+		// remove the existing flag file
+		err := os.Remove(FlagImageFile)
+		if err != nil {
+			terrors.Log(err, "error removing old flag image")
+		}
+	}
+
+	vid := CurrentlyPlaying
+	// find the next unflagged video
+	if vid.Flagged {
+		vid = vid.Next()
+	}
+
+	// this is the image we should be showing
+	newFlagFile := flagSourceFile(vid.State)
+
+	// if nothing was returned, we don't have an image to use
+	if newFlagFile == "" {
+		err := fmt.Errorf("no matching image found")
+		terrors.Log(err, "error creating new flag image")
+	}
+
+	// copy the image to the live location
+	err := os.Symlink(newFlagFile, FlagImageFile)
+	if err != nil {
+		terrors.Log(err, "error creating new flag image")
+	}
+}
+
+// flagSourceFile returns the full path to a flag image file
+func flagSourceFile(state string) string {
+	// convert it to an abbreviation
+	abbrev := helpers.StateToStateAbbrev(state)
+	// return nothing if nothing was found
+	if abbrev == "" {
+		return ""
+	}
+	// make it lowercase
+	abbrev = strings.ToLower(abbrev)
+	fileName := fmt.Sprintf("%s.png", abbrev)
+
+	return path.Join(helpers.ProjectRoot(), "assets/flags/medium", fileName)
 }
