e0a2519719fae38a16c3e5cc1e6c99d35be1271e
Dana Merrick <dana.merrick@postmates.com>
2020-09-06 01:47:39 +0000

    Moving to supervisor

--- script/container_startup.sh
+++ script/container_startup.sh
@@ -2,29 +2,43 @@
 
 # this script is the container entrypoint for the OBS container
 
-OUR_IP=$(hostname -i)
+cat << EOF > /etc/supervisor/conf.d/syslog.conf
+[program:syslog]
+command=command=/usr/sbin/syslog-ng -F
+auto_start=true
+autorestart=true
+stdout_logfile=/var/log/syslog
+stderr_logfile=/var/log/syslog
+EOF
 
-# start VNC server (Uses VNC_PASSWD Docker ENV variable)
-mkdir -p "$HOME/.vnc" && echo "$VNC_PASSWD" | vncpasswd -f > "$HOME/.vnc/passwd"
-vncserver :0 -localhost no -nolisten -rfbauth "$HOME/.vnc/passwd" -xstartup /opt/tripbot/script/x11vnc_entrypoint.sh
+cat << EOF > /etc/supervisor/conf.d/vnc.conf
+[program:vnc]
+directory=/opt/tripbot
+command=script/x11/start-vnc.sh
+auto_start=true
+autorestart=true
+stdout_logfile=syslog
+stderr_logfile=syslog
+EOF
 
-echo -e "\n\n------------------ OBS environment started ------------------"
-echo -e "\nVNC server started:\n\t=> connect via VNC at vnc://$OUR_IP:5900"
-echo -e "\nvlc-server started:\n\t=> connect via http://$VLC_SERVER_HOST/vlc/current\n"
-echo -e "\nOBS started:\n\t=> view at https://twitch.tv/$CHANNEL_NAME\n"
+cat << EOF > /etc/supervisor/conf.d/vlc.conf
+[program:vlc]
+directory=/opt/tripbot
+command=script/x11/start-vlc.sh
+auto_start=true
+autorestart=true
+stdout_logfile=syslog
+stderr_logfile=syslog
+EOF
 
-if [ ! -z "${STREAM_KEY}" ]; then
-  echo -e "\n\nStream is \e[31mLIVE\e[0m!" # red text
-fi
+cat << EOF > /etc/supervisor/conf.d/obs.conf
+[program:obs]
+directory=/opt/tripbot
+command=script/x11/start-obs.sh
+auto_start=true
+autorestart=true
+stdout_logfile=syslog
+stderr_logfile=syslog
+EOF
 
-if [ -z "$1" ]; then
-  # tail all the logs we care about
-  # (c.p. https://unix.stackexchange.com/a/195930/202812)
-  tail -F /opt/tripbot/log/vlc*.log \
-    | awk '/^==> / {a=substr($0, 5, length-8); next}
-                 {print a":"$0}'
-else
-  echo -e "\n\n------------------ EXECUTE COMMAND ------------------"
-  echo "Executing command: '$*'"
-  exec "$@"
-fi
+supervisord --nodaemon -c /etc/supervisor/supervisord.conf
