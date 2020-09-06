bdf8caed52687174d4cfbc2c450c952741b8eff2
Dana Merrick <dana.merrick@postmates.com>
2020-09-06 01:48:37 +0000

    Small tweak

--- script/x11/start-vnc.sh
+++ script/x11/start-vnc.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
 # start VNC server (Uses VNC_PASSWD Docker ENV variable)
-mkdir -p "$HOME/.vnc" && echo "$VNC_PASSWD" | vncpasswd -f > "$HOME/.vnc/passwd"
+mkdir -p "$HOME/.vnc" \
+  && echo "$VNC_PASSWD" | vncpasswd -f > "$HOME/.vnc/passwd"
+
 vncserver :0 -localhost no -nolisten -rfbauth "$HOME/.vnc/passwd" -xstartup /usr/bin/fluxbox
