695b92a52a1427bcf4ae2d7ea03aa81710ecfd58
Dana Merrick <dmerrick@users.noreply.github.com>
2020-12-05 16:54:21 -0500

    Removing old scripts (#41)

--- bin/.old/find-broken-vids.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-# this script finds video files that ffmpeg thinks are broken
-
-find -s . -name "2018_*.MP4" -exec bash -c 'echo "{}"; ffmpeg -v error -i "{}" -map 0:1 -f null - >>error.log 2>&1' \;
