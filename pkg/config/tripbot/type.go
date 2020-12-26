5ffce84c8df200b504d8d43a8bb3bc98a2815144
Dana Merrick <dana.merrick@gmail.com>
2020-12-25 22:48:27 -0500

    Fiiiine

--- pkg/config/tripbot/type.go
+++ pkg/config/tripbot/type.go
@@ -25,10 +25,11 @@ type TripbotConfig struct {
 	VideoDir string `default:"/opt/data/Dashcam/_all" envconfig:"VIDEO_DIR"`
 	// ScreencapDir is where we store full screenshots from the videos
 	ScreencapDir string `default:"/opt/data/screencaps" envconfig:"SCREENCAP_DIR"`
+
 	// CroppedCornersDir is where we store the cropped versions of screencaps (to OCR them)
 	CroppedCornersDir string `default:"/opt/data/cropped-corner" envconfig:"CROPPED_CORNERS_DIR"`
-	// // MapsOutputDir is where generated maps will be stored
-	// MapsOutputDir string `default:"/opt/data/maps" envconfig:"MAPS_OUTPUT_DIR"`
+	// MapsOutputDir is where generated maps will be stored
+	MapsOutputDir string `default:"/opt/data/maps" envconfig:"MAPS_OUTPUT_DIR"`
 
 	// TripbotPidFile is where the tripbot PID is written
 	TripbotPidFile string `default:"/opt/data/run/tripbot.pid" envconfig:"TRIPBOT_PIDFILE"`
