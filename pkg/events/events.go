8f4c9be8fa09b1ac605c914a70684f17ef23bbbc
Dana Merrick <dana.merrick@gmail.com>
2020-12-28 14:28:34 -0500

    Adding new error class

--- pkg/events/events.go
+++ pkg/events/events.go
@@ -6,6 +6,7 @@ import (
 
 	c "github.com/adanalife/tripbot/pkg/config/tripbot"
 	"github.com/adanalife/tripbot/pkg/database"
+	terrors "github.com/adanalife/tripbot/pkg/errors"
 	"github.com/logrusorgru/aurora"
 )
 
@@ -19,8 +20,7 @@ type Event struct {
 func Login(user string) error {
 	if c.Conf.ReadOnly && c.Conf.Verbose {
 		log.Printf("Not logging in %s because we're in read-only mode", aurora.Magenta(user))
-		//TODO: create error here
-		return nil
+		return &terrors.ReadOnlyError{Msg: "read-only mode"}
 	}
 	tx := database.Connection().MustBegin()
 	//TODO: do something with result here?
@@ -34,7 +34,7 @@ func Login(user string) error {
 func Logout(user string) error {
 	if c.Conf.ReadOnly && c.Conf.Verbose {
 		log.Printf("Not logging out %s because we're in read-only mode", aurora.Magenta(user))
-		return
+		return &terrors.ReadOnlyError{Msg: "read-only mode"}
 	}
 	tx := database.Connection().MustBegin()
 	//TODO: do something with result here?
