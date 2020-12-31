fb6d9be51c4f00135e5ad77c8fd166b6ce3c37b0
Dana Merrick <dana.merrick@gmail.com>
2020-12-31 13:23:51 -0500

    Bringing back leaderboard onscreen

--- pkg/onscreens-server/leaderboard.go
+++ pkg/onscreens-server/leaderboard.go
@@ -16,21 +16,8 @@ var Leaderboard *Onscreen
 func InitLeaderboard() {
 	log.Println("Creating leaderboard onscreen")
 	Leaderboard = New(leaderboardFile)
-	// go leaderboardLoop()
 }
 
 func ShowLeaderboard(content string) {
-
-	//TODO: re-enable this
-	log.Println("Not showing leaderboard")
-	// Leaderboard.ShowFor(content, leaderboardDuration)
+	Leaderboard.ShowFor(content, leaderboardDuration)
 }
-
-// func leaderboardLoop() {
-// 	for { // forever
-// 		if rand.Intn(10) == 0 {
-// 			ShowLeaderboard()
-// 		}
-// 		time.Sleep(time.Duration(30 * time.Second))
-// 	}
-// }
